909a8bf839314890054d6e037bef95e622df085d
Michaël Malter <dev@michaelmalter.fr>
2013-03-15 15:36:27 +0100

    Only return floats. Parse dates accordingly

--- dlstats.py
+++ dlstats.py
@@ -43,26 +43,26 @@ def INSEE(INSEE_id,name):
 
 	values_csv = TextIOWrapper(archive.open('Values.csv','rU'),encoding='latin-1',newline='')
 	if series_info['Periodicity'] == 'Annual':
-		data = pandas.read_table(values_csv,encoding='latin-1',sep=';',header=0,parse_dates=True,index_col=0,decimal='.',thousands=',',names=['Year',Name],skiprows=3)
+		data = pandas.read_table(values_csv,encoding='latin-1',sep=';',header=0,parse_dates=True,index_col=0,decimal='.',thousands=',',names=['Year',Name],skiprows=3, dtype='float64')
 		data = data.resample('A', fill_method='ffill')
 	if series_info['Periodicity'] == 'Quarterly':
 		def _quarterly_data_parser(year_and_quarter):
 			year,quarter = year_and_quarter.split()
-			return datetime.datetime.strptime(year + ' ' + str(int(quarter)*3),'%Y %m')
-		data = pandas.read_table(values_csv,encoding='latin-1',sep=';',header=0,parse_dates=[['Year','Quarter']],index_col=0,date_parser=_quarterly_data_parser,decimal='.',thousands=',',names=['Year','Quarter',name],skiprows=2)
+			return datetime.datetime.strptime(year + ' ' + str(int(quarter)*3),'%Y.0 %m.0')
+		data = pandas.read_table(values_csv,encoding='latin-1',sep=';',header=0,parse_dates=[['Year','Quarter']],index_col=0,date_parser=_quarterly_data_parser,decimal='.',thousands=',',names=['Year','Quarter',name],skiprows=2, dtype='float64')
 		data = data.resample('Q', fill_method='ffill')
 	if series_info['Periodicity'] == 'Monthly':
 		def _monthly_data_parser(year_and_month):
-			return datetime.datetime.strptime(year_and_month,'%Y %m')
-		data = pandas.read_table(values_csv,encoding='latin-1',sep=';',header=0,parse_dates=[['Year','Month']],index_col=0,date_parser=_monthly_data_parser,decimal='.',thousands=',',names=['Year','Month',name],skiprows=3)
+			return datetime.datetime.strptime(year_and_month,'%Y.0 %m.0')
+		data = pandas.read_table(values_csv,encoding='latin-1',sep=';',header=0,parse_dates=[['Year','Month']],index_col=0,date_parser=_monthly_data_parser,decimal='.',thousands=',',names=['Year','Month',name],skiprows=3,dtype='float64')
 		data = data.resample('M', fill_method='ffill')
-	data = data.tz_localize('Europe/Paris')
+	#data = data.tz_localize('Europe/Paris')
 	data.name = series_info['Heading']
 	data.index.name = 'Date'
-	data.columns = [name]
-	output = data[name]
-	output.name = name
-	return output
+	#data.columns = [name]
+	#output = data[name]
+	#output.name = name
+	return data
 
 
 
