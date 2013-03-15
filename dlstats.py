7bdaad49453bd780b0b9b50889070e82e846e98d
Michaël Malter <dev@michaelmalter.fr>
2013-03-15 15:48:46 +0100

    Corrected float conversion

--- dlstats.py
+++ dlstats.py
@@ -48,15 +48,16 @@ def INSEE(INSEE_id,name):
 	if series_info['Periodicity'] == 'Quarterly':
 		def _quarterly_data_parser(year_and_quarter):
 			year,quarter = year_and_quarter.split()
-			return datetime.datetime.strptime(year + ' ' + str(int(quarter)*3),'%Y.0 %m.0')
-		data = pandas.read_table(values_csv,encoding='latin-1',sep=';',header=0,parse_dates=[['Year','Quarter']],index_col=0,date_parser=_quarterly_data_parser,decimal='.',thousands=',',names=['Year','Quarter',name],skiprows=2, dtype='float64')
+			return datetime.datetime.strptime(year + ' ' + str(int(quarter)*3),'%Y %m')
+		data = pandas.read_table(values_csv,encoding='latin-1',sep=';',header=0,parse_dates=[['Year','Quarter']],index_col=0,date_parser=_quarterly_data_parser,decimal='.',thousands=',',names=['Year','Quarter',name],skiprows=2)
 		data = data.resample('Q', fill_method='ffill')
 	if series_info['Periodicity'] == 'Monthly':
 		def _monthly_data_parser(year_and_month):
-			return datetime.datetime.strptime(year_and_month,'%Y.0 %m.0')
-		data = pandas.read_table(values_csv,encoding='latin-1',sep=';',header=0,parse_dates=[['Year','Month']],index_col=0,date_parser=_monthly_data_parser,decimal='.',thousands=',',names=['Year','Month',name],skiprows=3,dtype='float64')
+			return datetime.datetime.strptime(year_and_month,'%Y %m')
+		data = pandas.read_table(values_csv,encoding='latin-1',sep=';',header=0,parse_dates=[['Year','Month']],index_col=0,date_parser=_monthly_data_parser,decimal='.',thousands=',',names=['Year','Month',name],skiprows=3)
 		data = data.resample('M', fill_method='ffill')
 	#data = data.tz_localize('Europe/Paris')
+	data = data.astype('float64')
 	data.name = series_info['Heading']
 	data.index.name = 'Date'
 	#data.columns = [name]
