c6483549dcdf82002056611f75657cc5d53e73d3
Michaël Malter <dev@michaelmalter.fr>
2013-05-14 16:33:44 +0200

    Dumb commit 	modified:   INE.py

--- INE.py
+++ INE.py
@@ -11,29 +11,73 @@ from io import BytesIO, StringIO, TextIOWrapper
 import urllib.request, http.cookiejar
 import re
 
-response1 = urllib.request.urlopen("http://www.ine.es/en/inebmenu/indice_en.htm")
-webpage = response1.read()
-webpage = webpage.decode("ISO-8859-15")
-webpage = lxml.html.fromstring(webpage)
-regex = re.compile('/jaxi/menu.do\?type=pcaxis&path=(.*)&file=inebase&L=1')
-categories = {}
-for table in webpage.iterfind(".//table"):
-    if table.get("summary") == "Table with the complete list of INEbase statistical operations":
-        for line in table.iterfind(".//td"):
-            lineclass = line.get("class")
-            if lineclass == "diez":
-                for anchor in line.iterfind(".//a"):
-                    current_category = anchor.text
-                    if current_category != None:
-                        categories[current_category] = {}
-            for anchor in line.iterfind(".//a"):
-                href = anchor.get("href")
-                if href != None:
-                    match = regex.findall(href)
-                    if match != []:
-                        if current_category != None:
-                            categories[current_category][anchor.text] = match[0]
+def open_url(url):
+    response = urllib.request.urlopen(url)
+    webpage = response.read()
+    webpage = webpage.decode("ISO-8859-15")
+    return lxml.html.fromstring(webpage)
 
+def make_url_absolute(url):
+    regex = re.compile('/')
+    is_absolute_url = regex.match(url)
+    if is_absolute_url != None:
+        absolute_url = "http://www.ine.es" + url
+    else:
+        absolute_url = url
+    return absolute_url
+
+class INE(object):
+    def __init__(self):
+        webpage = open_url("http://www.ine.es/en/inebmenu/indice_en.htm")
+        regex1 = re.compile('/jaxi/menu.do\?type=pcaxis&path=(.*)&file=inebase&L=1')
+        regex2 = re.compile('%2F')
+        regex3 = re.compile('(/jaxi/menu.do\?)|(/jaxiBD/menu.do\?)|(http://www.ine.es/jaxi/menu.do\?)|(http://www.ine.es/jaxiBD/menu.do\?)')
+        regex4 = re.compile('((.htm)|(.pdf))$')
+        categories = {}
+        for table in webpage.iterfind(".//table"):
+            if table.get("summary") == "Table with the complete list of INEbase statistical operations":
+                for line in table.iterfind(".//td"):
+                    lineclass = line.get("class")
+                    if lineclass == "diez":
+                        for anchor in line.iterfind(".//a"):
+                            current_category = anchor.text
+                            if current_category != None:
+                                categories[current_category] = {}
+                    for anchor in line.iterfind(".//a"):
+                        href = anchor.get("href")
+                        if href != None:
+                            match = regex1.findall(href)
+                            if match != []:
+                                if current_category != None:
+                                    categories[current_category][anchor.text] = re.sub("%2F","/",match[0])
+        self.categories = {}
+        for l1category, l1value in categories.items():
+            for l2category, l2value in l1value.items():
+                webpage = open_url("http://www.ine.es/jaxi/menu.do?type=pcaxis&path=" + l2value + "&file=inebase&L=1")
+                for table in webpage.iterfind(".//table"):
+                    if table.get("class") == "MENUJAXI":
+                        l3values = []
+                        for anchor in table.iterfind(".//a"):
+                            href = anchor.get("href")
+                            match = regex3.match(href)
+                            if match != None:
+                                l3values.append(make_url_absolute(match.string)) 
+                        l4values = {}
+                        for l3value in l3values:
+                            print(l3value)
+                            webpage_ = open_url(l3value)
+                            for table_ in webpage_.iterfind(".//table"):
+                                if table_.get("class") == "MENUJAXI":
+                                    for anchor_ in table_.iterfind(".//a"):
+                                        if anchor_.get("class") == "sinsubrayar":
+                                            if not regex4.search(anchor_.get("href")):
+                                                l4values[anchor_.text.strip()] = make_url_absolute(anchor_.get("href"))
+                        print(l4values)
+               # self.categories[l1category][l2category][l3category] = l3value
+                
+        
+
+print("1")
 cookie = http.cookiejar.CookieJar()
 opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie))
 response2 = opener.open("http://www.ine.es/jaxi/tabla.do?path=/t38/bme2/t30/p149/l1/&file=0907001.px&type=pcaxis&L=1")
@@ -49,6 +93,7 @@ for form in webpage.iterfind(".//form"):
             POST_request.append((select.get("name"), option.get("value")))
 # By default the INE presents data in rows. We (and pandas) don't like that and
 # permutate the POST variables.
+print("2")
 rows = []
 columns = []
 i = 0
