deb2566d8be82e49b3280f7f381f7b9e8c0aa641
Michaël Malter <dev@michaelmalter.fr>
2013-06-05 03:04:42 +0200

    Added metadatas to the setup script 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from distutils.core import setup
 setup(name='dlstats',
 	version='0.1',
-	py_modules=['dlstats'],
+    description='A python module that provides an interface between statistics providers and pandas.',
+    author='Michaël Malter',
+    author_email='dev@michaelmalter.fr'
+    package_dir={'': 'src'},
+    packages=[''],
+    install_requires=[
+        'pandas>=0.11'
+      ]
 	)
