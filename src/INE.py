3370772c77828fb949530f0eb66658c180142687
Michaël Malter <michael.malter@cepremap.org>
2013-06-05 13:22:05 +0000

    More practical file hierarchy 	new file:   src/INE.py 	new file:   src/dlstats.py

---
+++ src/INE.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import lxml.html
+import numpy
+from pandas.tseries.offsets import *
+import pandas
+import datetime
+import os
+from io import BytesIO, StringIO, TextIOWrapper
+import urllib.request, http.cookiejar
+import re
+
+def open_url(url):
+    response = urllib.request.urlopen(url)
+    webpage = response.read()
+    webpage = webpage.decode("ISO-8859-15")
+    return lxml.html.fromstring(webpage)
+
+def make_url_absolute(url):
+    regex = re.compile('/')
+    is_absolute_url = regex.match(url)
+    if is_absolute_url != None:
+        absolute_url = "http://www.ine.es" + url
+    else:
+        absolute_url = url
+    return absolute_url
+
+class INE(object):
+    def __init__(self):
+        webpage = open_url("http://www.ine.es/en/inebmenu/indice_en.htm")
+        regex1 = re.compile('/jaxi/menu.do\?type=pcaxis&path=(.*)&file=inebase&L=1')
+        regex2 = re.compile('%2F')
+        regex3 = re.compile('(/jaxi/menu.do\?)|(/jaxiBD/menu.do\?)|(http://www.ine.es/jaxi/menu.do\?)|(http://www.ine.es/jaxiBD/menu.do\?)')
+        regex4 = re.compile('((.htm)|(.pdf))$')
+        categories = {}
+        for table in webpage.iterfind(".//table"):
+            if table.get("summary") == "Table with the complete list of INEbase statistical operations":
+                for line in table.iterfind(".//td"):
+                    lineclass = line.get("class")
+                    if lineclass == "diez":
+                        for anchor in line.iterfind(".//a"):
+                            current_category = anchor.text
+                            if current_category != None:
+                                categories[current_category] = {}
+                    for anchor in line.iterfind(".//a"):
+                        href = anchor.get("href")
+                        if href != None:
+                            match = regex1.findall(href)
+                            if match != []:
+                                if current_category != None:
+                                    categories[current_category][anchor.text] = re.sub("%2F","/",match[0])
+        self.categories = {}
+        for l1category, l1value in categories.items():
+            for l2category, l2value in l1value.items():
+                webpage = open_url("http://www.ine.es/jaxi/menu.do?type=pcaxis&path=" + l2value + "&file=inebase&L=1")
+                for table in webpage.iterfind(".//table"):
+                    if table.get("class") == "MENUJAXI":
+                        l3values = []
+                        for anchor in table.iterfind(".//a"):
+                            href = anchor.get("href")
+                            match = regex3.match(href)
+                            if match != None:
+                                l3values.append(make_url_absolute(match.string)) 
+                        l4values = {}
+                        for l3value in l3values:
+                            print(l3value)
+                            webpage_ = open_url(l3value)
+                            for table_ in webpage_.iterfind(".//table"):
+                                if table_.get("class") == "MENUJAXI":
+                                    for anchor_ in table_.iterfind(".//a"):
+                                        if anchor_.get("class") == "sinsubrayar":
+                                            if not regex4.search(anchor_.get("href")):
+                                                l4values[anchor_.text.strip()] = make_url_absolute(anchor_.get("href"))
+                        print(l4values)
+               # self.categories[l1category][l2category][l3category] = l3value
+                
+        
+
+print("1")
+cookie = http.cookiejar.CookieJar()
+opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie))
+response2 = opener.open("http://www.ine.es/jaxi/tabla.do?path=/t38/bme2/t30/p149/l1/&file=0907001.px&type=pcaxis&L=1")
+webpage = response2.read()
+webpage = webpage.decode("ISO-8859-15")
+webpage = lxml.html.fromstring(webpage)
+POST_request = []
+for form in webpage.iterfind(".//form"):
+    for input in form.iterfind(".//input"):
+        POST_request.append((input.get("name"), input.get("value")))
+    for select in form.iterfind(".//select"):
+        for option in select.iterfind(".//option"):
+            POST_request.append((select.get("name"), option.get("value")))
+# By default the INE presents data in rows. We (and pandas) don't like that and
+# permutate the POST variables.
+print("2")
+rows = []
+columns = []
+i = 0
+while i < len(POST_request):
+    if POST_request[i][0] == 'rows':
+        rows.append(POST_request[i][1])
+        del POST_request[i]
+    if POST_request[i][0] == 'columns':
+        columns.append(POST_request[i][1])
+        del POST_request[i]
+    i += 1
+for item in columns:
+    POST_request.append(('rows',item))
+for item in rows:
+    POST_request.append(('columns',item))
+POST_request = urllib.parse.urlencode(POST_request)
+POST_request = POST_request.encode("ISO-8859-15")
+response3 = opener.open("http://www.ine.es/jaxiBD/tabla.do", POST_request)
+webpage = response3.read()
+webpage = webpage.decode("ISO-8859-15")
+webpage = lxml.html.fromstring(webpage)
+POST_request = []
+for form in webpage.iterfind(".//form"):
+    if form.get("action") == "download.do" and form.get("name") == "formDownloadCabecera":
+        for input in form.iterfind(".//input"):
+            if input.get("name") != "descargarformato":
+                POST_request.append((input.get("name"), input.get("value")))
+    else:
+        pass
+POST_request.append(("typeDownload", "4"))
+POST_request.append(("descargarformato", "Go"))
+POST_request = urllib.parse.urlencode(POST_request)
+response4 = opener.open("http://www.ine.es/jaxiBD/download.do?"+POST_request)
+webpage = response4.read()
+webpage = webpage.decode("ISO-8859-15")
+csv = StringIO(webpage)
+output = pandas.read_csv(csv,header=7, sep=',', skip_footer=11, skiprows=[0,1,2,3,4,5,6,8], parse_dates=True, index_col=0)
---
+++ src/dlstats.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import numpy
+from pandas.tseries.offsets import *
+import pandas
+import datetime
+import os
+from io import BytesIO, StringIO, TextIOWrapper
+import zipfile
+import urllib.request
+
+def INSEE(INSEE_id,name):
+	INSEE_id = INSEE_id
+	"""INSEE identifier on the BDM (www.insee.fr)"""
+#TODO : validate INSEE id by checking on the website
+#TODO : Network failure exception
+	response = urllib.request.urlopen(
+			"http://www.bdm.insee.fr/bdm2/exporterSeries.action?"
+			"periodes=toutes&nbPeriodes=0&liste_formats=txt&request_locale=en"
+			"&chrono=true&idbank="+INSEE_id)
+	memzip = BytesIO(response.read())
+	archive = zipfile.ZipFile(memzip, mode='r')
+	"""ZipFile provided by the INSEE"""
+
+	charact_csv = TextIOWrapper(archive.open('Charact.csv','rU'),encoding='latin-1',newline='')
+	carac_list = [line.strip() for line in charact_csv]
+	series_info={}
+	"""Various unreliable info on the series"""
+	for line in carac_list:
+		vars = line.split(';')
+		if vars != ['']:
+			series_info[vars[0]] = vars[1]
+
+	values_csv = TextIOWrapper(archive.open('Values.csv','rU'),encoding='latin-1',newline='')
+	if series_info['Periodicity'] == 'Annual':
+		data = pandas.read_table(values_csv,encoding='latin-1',sep=';',header=0,parse_dates=True,index_col=0,decimal='.',thousands=',',names=['Year',Name],skiprows=3, dtype='float64')
+		data = data.resample('A', fill_method='ffill')
+	if series_info['Periodicity'] == 'Quarterly':
+		def _quarterly_data_parser(year_and_quarter):
+			year,quarter = year_and_quarter.split()
+			return datetime.datetime.strptime(year + ' ' + str(int(quarter)*3),'%Y %m')
+		data = pandas.read_table(values_csv,encoding='latin-1',sep=';',header=0,parse_dates=[['Year','Quarter']],index_col=0,date_parser=_quarterly_data_parser,decimal='.',thousands=',',names=['Year','Quarter',name],skiprows=2)
+		data = data.resample('Q', fill_method='ffill')
+	if series_info['Periodicity'] == 'Monthly':
+		def _monthly_data_parser(year_and_month):
+			return datetime.datetime.strptime(year_and_month,'%Y %m')
+		data = pandas.read_table(values_csv,encoding='latin-1',sep=';',header=0,parse_dates=[['Year','Month']],index_col=0,date_parser=_monthly_data_parser,decimal='.',thousands=',',names=['Year','Month',name],skiprows=3)
+		data = data.resample('M', fill_method='ffill')
+	#data = data.tz_localize('Europe/Paris')
+	data = data.astype('float64')
+	data.name = series_info['Heading']
+	data.index.name = 'Date'
+	#data.columns = [name]
+	#output = data[name]
+	#output.name = name
+	return data
+
+
+
