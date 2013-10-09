cfc2da28e0a89dfeefe264d1b894a0628ed293c1
Michaël Malter <dev@michaelmalter.fr>
2013-10-09 18:02:02 +0200

    Refactoring. Daemon files.
    	nouveau : examples/config
    	nouveau : init/dlstats
    	nouveau : init/dlstats.py
    	nouveau : src/INSEE.py
    	nouveau : src/misc_func.py
    	nouveau : src/skeleton.py
    	modifié : setup.py
    	effacé :  src/dlstats.py
    	effacé :  src/functions.py

---
+++ src/skeleton.py
@@ -0,0 +1,13 @@
+class Skeleton(object):
+    def __init__(self):
+        pass
+    def update_categories_db(self):
+        """Update the categories in MongoDB
+        """
+        raise NotImplementedError("All the methods from the Skeleton class must
+                                  be implemented.")
+    def update_series_db(self):
+        """Update the series in MongoDB
+        """
+        raise NotImplementedError("All the methods from the Skeleton class must
+                                  be implemented.")
