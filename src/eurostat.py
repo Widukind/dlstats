cfb27963a5f7403bb333b7c52f787218bdd6c7a1
pchuong <pchuong@uranus.lan.cepremap.org>
2013-10-25 16:14:06 +0200

    Fixed collection name

--- src/eurostat.py
+++ src/eurostat.py
@@ -19,23 +19,23 @@ class eurostat(skeleton):
                         if title.attrib.values()[0] == 'en':
                             _categories=[title.text]
                             node={'name': title.text}
-                            _id = collection.insert(node)
-                            collection.update({'_id': parent_id}, {'$push': {'children': _id}})
+                            _id = self.db.categories.insert(node)
+                            self.db.categories.update({'_id': parent_id}, {'$push': {'children': _id}})
                     walktree(branch, title.text,_id)
                 for leaf in children.iterchildren('{urn:eu.europa.ec.eurostat.navtree}leaf'):
                     for title in leaf.iterchildren('{urn:eu.europa.ec.eurostat.navtree}title'):
                         if title.attrib.values()[0] == 'en':
                             _categories=[title.text]
                             node={'name': title.text}
-                            _id = collection.insert(node)
-                            collection.update({'_id': parent_id}, {'$push': {'children': _id}})
+                            _id = self.db.categories.insert(node)
+                            self.db.categories.update({'_id': parent_id}, {'$push': {'children': _id}})
 
         root=self.table_of_contents.iterfind('{urn:eu.europa.ec.eurostat.navtree}branch')
         for branch in root:
             for title in branch.iterchildren('{urn:eu.europa.ec.eurostat.navtree}title'):
                 if title.attrib.values()[0] == 'en':
                     node={'name': title.text}
-                    _id = collection.insert(node)
+                    _id = self.db.categories.insert(node)
             walktree(branch, title.text,_id)
     def update_series_db(self):
         """Update the series in MongoDB
