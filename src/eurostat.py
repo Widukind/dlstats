87bdf6a0e1f24a29eee01e763de218dbf73e91d7
Michaël Malter <michael.malter@cepremap.org>
2013-10-25 12:18:08 +0000

    Implementation of the eurostat update_categories_db method

--- src/eurostat.py
+++ src/eurostat.py
@@ -1,18 +1,42 @@
 from skeleton import Skeleton
-import lxml.html
+import lxml
 import urllib
 import misc_func
-import xml.etree.ElementTree as ET
 
 class eurostat(skeleton):
     def __init__(self):
-        self.bulk_dl = urllib.request.urlopen('http://www.epp.eurostat.ec.europa.eu/NavTree_prod/everybody/BulkDownloadListing?sort=1&file=table_of_contents.xml', timeout=7)
+        self.db = self.client.eurostat
+        table_of_contents = urllib.request.urlopen('http://www.epp.eurostat.ec.europa.eu/NavTree_prod/everybody/BulkDownloadListing?sort=1&file=table_of_contents.xml', timeout=7)
+        table_of_contents = webpage.read()
+        self.table_of_contents = lxml.etree.fromstring(table_of_contents)
     def update_categories_db(self):
         """Update the categories in MongoDB
         """
-        for elem in tree.iterfind('.//ns:leaf/ns:title/[@language="en"]',
-                                 {'ns':'urn:eu.europa.ec.eurostat.navtree'}):
-            print (elem.text)
+        def walktree(branch,category_name,parent_id ): 
+            for children in branch.iterchildren('{urn:eu.europa.ec.eurostat.navtree}children'):
+                for branch in children.iterchildren('{urn:eu.europa.ec.eurostat.navtree}branch'):
+                    for title in branch.iterchildren('{urn:eu.europa.ec.eurostat.navtree}title'):
+                        if title.attrib.values()[0] == 'en':
+                            _categories=[title.text]
+                            node={'name': title.text}
+                            _id = collection.insert(node)
+                            collection.update({'_id': parent_id}, {'$push': {'children': _id}})
+                    walktree(branch, title.text,_id)
+                for leaf in children.iterchildren('{urn:eu.europa.ec.eurostat.navtree}leaf'):
+                    for title in leaf.iterchildren('{urn:eu.europa.ec.eurostat.navtree}title'):
+                        if title.attrib.values()[0] == 'en':
+                            _categories=[title.text]
+                            node={'name': title.text}
+                            _id = collection.insert(node)
+                            collection.update({'_id': parent_id}, {'$push': {'children': _id}})
+
+        root=self.table_of_contents.iterfind('{urn:eu.europa.ec.eurostat.navtree}branch')
+        for branch in root:
+            for title in branch.iterchildren('{urn:eu.europa.ec.eurostat.navtree}title'):
+                if title.attrib.values()[0] == 'en':
+                    node={'name': title.text}
+                    _id = collection.insert(node)
+            walktree(branch, title.text,_id)
     def update_series_db(self):
         """Update the series in MongoDB
         """
