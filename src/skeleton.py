0a4320fa68b58cad59320c1170565696b65bb4bf
Michaël Malter <michael.malter@cepremap.org>
2013-11-15 18:32:43 +0000

    Cosmetic

--- src/skeleton.py
+++ src/skeleton.py
@@ -6,10 +6,10 @@ class Skeleton(object):
     def update_categories_db(self):
         """Update the categories in MongoDB
         """
-        raise NotImplementedError("All the methods from the Skeleton class must"
+        raise NotImplementedError("This method from the Skeleton class must"
                                   "be implemented.")
     def update_series_db(self):
         """Update the series in MongoDB
         """
-        raise NotImplementedError("All the methods from the Skeleton class must"
+        raise NotImplementedError("This method from the Skeleton class must"
                                   "be implemented.")
