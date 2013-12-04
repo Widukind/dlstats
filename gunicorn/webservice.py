6f05a55f342d51ef64ff7d3d06ba166ac1044f6b
pierre <pchuong3@users.noreply.github.com>
2013-12-04 14:54:10 +0100

    Update webservice.py
    return categories from MongoDB

--- gunicorn/webservice.py
+++ gunicorn/webservice.py
@@ -1,38 +1,48 @@
 #!/usr/bin/python3
 import flask
 import pymongo
-
 from flask.ext.restful import Api
+from flask import Flask
 from werkzeug.contrib.fixers import ProxyFix
+import bson
+from bson import json_util
+from bson.json_util import dumps
 
 #application instance
-app = flask.Flask(__name__)
+App = flask.Flask(__name__)
 
 # our api instance
-api=Api(app)
+api=Api(App)
 
-#client=connection = MongoClient()
 client = pymongo.MongoClient()
 
-#a ressource class that will be mapped to a URL
-# list_dbs derived from Resource
 class list_dbs(flask.ext.restful.Resource):
     def get(self):
         return client.database_names()
 
 class list_cat(flask.ext.restful.Resource):
     def get(self):
-        return client.INSEE.collection_names()
+        #return client.INSEE.collection_names()
+        return client.eurostat.collection_names()
 
-# map the list_dbs ressource to '/list_dbs' endpoint
+class cat_eurostat(flask.ext.restful.Resource):
+    def get(self):
+        #item = client.eurostat.categories3.find_one({"name2" : "Database by themes"})
+        item = client.eurostat.categories.find()
+        return dumps(item)
+
 api.add_resource(list_dbs, '/list_dbs')
 api.add_resource(list_cat, '/list_cat')
+api.add_resource(cat_eurostat, '/cat_eurostat')
 
-@app.route('/')
+@App.route('/')
 def index():
     return "test index!"
 
-app.wsgi_app = ProxyFix(app.wsgi_app)
+App.wsgi_app = ProxyFix(App.wsgi_app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    App.run(debug=True)
+
+#gunicorn --bind=127.0.0.1:8001 webservice:App 
+#curl -i http://127.0.0.1:8001/cat_eurostat
