a4d2cb0edc19cdffcb68c9f1987074be6bf0ea0c
pierre <pchuong3@users.noreply.github.com>
2014-02-14 10:37:27 +0100

    tree comparison in order
    tree comparison in order

--- src/fetchers/unitest.py
+++ src/fetchers/unitest.py
@@ -24,6 +24,32 @@ class TestEurostat(unittest.TestCase):
         datatstat=self.toto.db.command("dbstats")
         for key, value in datatstat.items():
             print( key, value)
+            
+    def test_allelement(self):
+        datatest=list(self.toto.db.categories.find())
+        data = []
+        with open("mjson.json") as f:
+            data =[json.loads(line) for line in f]
+        dataname=[]
+        dataname=[r.get("name", "Missing: name") for r in data]
+        rootid=self.toto.db.categories.find_one({"name": "Database by themes"})["_id"]
+        rootchildid=self.toto.db.categories.find_one({"_id": rootid})
+        testrootid=[r.get("_id", "Missing: _id") for r in data if r.get("name", "Missing: name")=='Database by themes']
+        print('testrootid',testrootid)
+        def testtree(idparent,testidparent):
+            parentname=self.toto.db.categories.find_one({"_id": idparent})["name"]
+            testparentname=[r.get("name", "Missing: name") for r in data if r.get("_id", "Missing: _id")==testidparent[0]][0]
+            self.assertTrue(testparentname==parentname)
+            try:
+                childid=self.toto.db.categories.find_one({"_id": idparent})["children"]
+                testchildid=[r.get("children", "Missing: children")[0] for r in data if r.get("_id", "Missing: _id")==testidparent[0]]
+                print('testchildid:',testchildid)     
+                for children in childid:
+                    testtree(children,testchildid)
+            except Exception:
+                pass
+        
+        testtree(rootid,testrootid)            
 
     def test_Nbelement(self):
         mongo_NBm=self.toto.db.categories.find().count()    
