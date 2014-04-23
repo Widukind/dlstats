a03eef3968c7a5fc911a20dd65ba9dcd867e3b58
michael <michael@zen>
2014-04-23 12:33:44 +0200

    Refactored configuration file handler

--- src/configuration.py
+++ src/configuration.py
@@ -1,30 +1,29 @@
-import ConfigParser
+"""Cross platform configuration file handler.
+
+This module manages dlstats configuration files, providing
+easy access to the options."""
+
+import configparser
 import os
 
-class ConfigDlstats(object):
-    """Cross platform configuration file handler.
-
-    This class manages dlstats configuration files, providing
-    easy access to the options."""
-
-    def __init__(self):
-        """Open the configuration files handler, choosing the right
-        path depending on the platform."""
-        appname = 'dlstats'
-        if os.name == 'posix':
-            if os.path.isfile(os.environ["HOME"]+'/.'+appname):
-                self.filename = os.environ["HOME"]+'/.'+appname
-            elif os.path.isfile('/etc/'+appname):
-                self.filename = '/etc/'+appname
-            else:
-                raise FileNotFoundError('No configuration file found.')
-        elif os.name == 'mac':
-            self.filename = ("%s/Library/Application Support/%s" %
-                        (os.environ["HOME"], appname))
-        elif os.name == 'nt':
-            self.filename = ("%s\Application Data\%s" %
-                        (os.environ["HOMEPATH"], appname))
+def get_filename():
+    """Return the configuration file path."""
+    appname = 'dlstats'
+    if os.name == 'posix':
+        if os.path.isfile(os.environ["HOME"]+'/.'+appname):
+            return os.environ["HOME"]+'/.'+appname
+        elif os.path.isfile('/etc/'+appname):
+            return '/etc/'+appname
         else:
-            raise UnsupportedOSError(os.name)
-    self.config = ConfigParser.ConfigParser()
-    self.config.read(self.filename)
+            raise FileNotFoundError('No configuration file found.')
+    elif os.name == 'mac':
+        return ("%s/Library/Application Support/%s" % (os.environ["HOME"], appname))
+    elif os.name == 'nt':
+        return ("%s\Application Data\%s" % (os.environ["HOMEPATH"], appname))
+    else:
+        raise UnsupportedOSError(os.name)
+
+filename = get_filename()
+
+config = configparser.ConfigParser()
+config.read(filename)
