2e5a16affa3540185eb7e8ba94413ca0c6e25ef4
michael <michael@zen>
2014-04-24 23:44:58 +0200

    Make private attributes private

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,27 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-from . import fetchers, gunicorn, misc_func, configuration
+from . import fetchers, gunicorn, misc_func
+import configparser
+import os
+
+def _get_filename():
+    """Return the configuration file path."""
+    appname = 'dlstats'
+    if os.name == 'posix':
+        if os.path.isfile(os.environ["HOME"]+'/.'+appname):
+            return os.environ["HOME"]+'/.'+appname
+        elif os.path.isfile('/etc/'+appname):
+            return '/etc/'+appname
+        else:
+            raise FileNotFoundError('No configuration file found.')
+    elif os.name == 'mac':
+        return ("%s/Library/Application Support/%s" % (os.environ["HOME"], appname))
+    elif os.name == 'nt':
+        return ("%s\Application Data\%s" % (os.environ["HOMEPATH"], appname))
+    else:
+        raise UnsupportedOSError(os.name)
+
+_filename = _get_filename()
+
+config = configparser.ConfigParser()
+config.read(_filename)
