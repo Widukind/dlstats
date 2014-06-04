92b657566f7236992061a5d620bd1138d35d4f7a
Michaël Malter <dev@michaelmalter.fr>
2014-06-04 11:24:04 +0200

    Add systemd unit
    We are still managing the creation of the socket in python. We want to
    configure the path to the socket in python.

--- setup.py
+++ setup.py
@@ -11,14 +11,15 @@ setup(name='dlstats',
     url='https://github.com/Widukind', 
       package_dir={'dlstats': 'package', 'dlstats.fetchers': 'package/fetchers'},
     packages=['dlstats', 'dlstats.fetchers'],
-    data_files=[('/usr/local/bin',['server/dlstats_server.py'])],
+    data_files=[('/usr/local/bin',['server/dlstats_server.py']),
+                ('/etc/systemd/system',['server/dlstats.service'])],
     install_requires=[
         'pandas>=0.12',
       ]
 	)
 
-with open('/etc/init.d/dlstats'):
-        os.chmod('/etc/init.d/dlstats', 0o755)
+with open('/etc/systemd/system/dlstats.service'):
+        os.chmod('/etc/systemd/system/dlstats.service', 0o755)
 
-with open('/usr/local/bin/dlstats_daemon.py'):
-        os.chmod('/usr/local/bin/dlstats_daemon.py', 0o755)
+with open('/usr/local/bin/dlstats_server.py'):
+        os.chmod('/usr/local/bin/dlstats_server.py', 0o755)
