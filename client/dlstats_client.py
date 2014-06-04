ada079e7ffb1b2450a5d0c46e86e577334740c50
Michaël Malter <dev@michaelmalter.fr>
2014-06-04 12:02:45 +0200

    First shot at a client for dlstats

---
+++ client/dlstats_client.py
@@ -0,0 +1,11 @@
+import socket
+import configobj
+from dlstats import configuration
+
+socket_path = os.path.normpath(configuration.['socket_directory'] 'dlstats.socket'):
+client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+client.connect(socket_path)
+client.send("Hello world!")
+response = client.recv(512)
+print(response)
+client.close()
