16d57fa810f160b51fbaef77fda8e38c3c0c9508
Michaël Malter <dev@michaelmalter.fr>
2014-06-11 04:48:17 +0200

    Fix a few typos. Example of client/server communication.

--- package/client.py
+++ package/client.py
@@ -1,11 +1,12 @@
 import socket
 import configobj
+import os
 from dlstats import configuration
 
-socket_path = os.path.normpath(configuration['General']['socket_directory'] 'dlstats.socket'):
+socket_path = os.path.normpath(configuration['General']['socket_directory']+'/dlstats.socket')
 client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 client.connect(socket_path)
-client.send("Hello world!")
+client.send(b"hello")
 response = client.recv(512)
-print(response)
+print(response.decode())
 client.close()
--- package/server.py
+++ package/server.py
@@ -8,7 +8,7 @@ import time
 def get_logger(configuration):
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
-    file_handler = logging.FileHandler(os.path.normpath(configuration['General']['logging_directory']) 'dlstats.log')
+    file_handler = logging.FileHandler(os.path.normpath(configuration['General']['logging_directory'])+'/dlstats.log')
     file_handler.setLevel(logging.DEBUG)
     frmt = logging.Formatter(
         '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -18,29 +18,33 @@ def get_logger(configuration):
 
 logger = get_logger(configuration)
 
+def hello():
+    return ('I love you won\'t you tell me your name?')
+
 def event_loop(configuration):
     logger.info('Spawning event loop.')
-    socket_path = os.path.normpath(configuration['General']['socket_directory'] 'dlstats.socket'):
+    socket_path = os.path.normpath(configuration['General']['socket_directory']+'/dlstats.socket')
     if os.path.exists( socket_path ):
           os.remove( socket_path )
     server = socket.socket( socket.AF_UNIX, socket.SOCK_STREAM )
     server.bind(socket_path)
     server.listen(5)
+    commands = {'hello':hello}
     exit_sentinel = False
     while True: 
         connection, address = server.accept()
-        while True
-            data = connection.recv( 512 )
+        while True:
+            data = connection.recv( 512 ).decode()
             if not data or data == 'close':
                 break
             else:
                 logger.info('Received command: %s', data)
                 if data == 'quit':
                     exit_sentinel = True
-                elif data in commands:
-                    connection.send(commands[data])
+                elif data in commands.keys():
+                    connection.send(commands[data]().encode())
                 else:
-                    connection.send('Unrecognized command: ' data)
+                    connection.send('Unrecognized command: '+data)
         connection.close()
         if exit_sentinel == True:
             break
