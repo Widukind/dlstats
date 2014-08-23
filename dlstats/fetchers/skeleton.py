0815606f1351e1d2f6665c89a33f471c2f0d0c39
Michel Juillard <michel.juillard@mjui.fr>
2014-08-23 16:52:56 +0200

    added functions to update database

--- dlstats/fetchers/skeleton.py
+++ dlstats/fetchers/skeleton.py
@@ -28,3 +28,52 @@ class Skeleton(object):
         """
         raise NotImplementedError("This method from the Skeleton class must"
                                   "be implemented.")
+    def bson_update(self,coll,bson,key):
+        old_bson = coll.find_one({key: bson[key]})
+        if old_bson == None:
+            _id = coll.insert(bson)
+            return _id
+        else:
+            identical = True
+            for k in bson.keys():
+                if not (k == 'versionDate'):
+                    if (old_bson[k] != bson[k]):
+                        self.log_warning(coll.database.name+'.'+coll.name+': '+k+" has changed value. Old value: {}, new value: {}".format(old_bson[k],bson[k]))
+                        identical = False
+            if not identical:
+                coll.update({'_id': old_bson['_id']},bson)
+            return old_bson['_id']
+
+    def series_update(self,coll,bson,key):
+        old_bson = coll.find_one({key: bson[key]})
+        if old_bson == None:
+            _id = coll.insert(bson)
+            return _id
+        else:
+            identical = True
+            for k in bson.keys():
+                if (k != 'versionDate'):
+                    if (old_bson[k] != bson[k]):
+                        self.log_warning(coll.database.name+'.'+coll.name+': '+k+" has changed value. Old value: {}, new value: {}".format(old_bson[k],bson[k]))
+                        identical = False
+            if not identical:
+                values = bson['values']
+                old_values = old_bson['values']
+                releaseDates = bson['releaseDates']
+                old_releaseDates = old_bson['releaseDates']
+                old_revisions = old_bson['revisions']
+                for i in range(len(old_values)):
+                    if old_values[i] == values[i]:
+                        releaseDates[i] = old_releaseDates[i]
+                    else:
+                        revisions[i] = old_revisions[i]
+                        revisions[i][releaseDates[i]] = old_values[i]
+                bson['releaseDates'] = releaseDates
+                bson['revisions'] = revisions
+                coll.update({'_id': old_bson['_id']},bson,upsert=True)
+            return old_bson['_id']
+
+    def log_warning(self,msg):
+        """Send message to the database operator"""
+
+        print(msg)
