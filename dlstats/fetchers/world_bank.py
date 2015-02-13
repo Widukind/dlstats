d4de1ff47006a625c930ad3307315d801b1d00be
Salimeh <s.moniri@gmail.com>
2015-02-13 17:47:06 +0100

    Fix dimensions_list

--- dlstats/fetchers/world_bank.py
+++ dlstats/fetchers/world_bank.py
@@ -5,46 +5,75 @@ import urllib.request
 import xlrd
 import os
 import time
+import datetime
 import pandas
 import logging
-from datetime import datetime
-import pprint
+import random
 
 class WorldBank(Skeleton):
     def __init__(self):
-        super().__init__() 
-        self.lgr = logging.getLogger('WorldBank')
-        self.lgr.setLevel(logging.DEBUG)
-        self.fh = logging.FileHandler('WorldBank.log')
-        self.fh.setLevel(logging.DEBUG)
-        self.frmt = logging.Formatter(
-            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        self.fh.setFormatter(self.frmt)
-        self.lgr.addHandler(self.fh)
-        self.lgr.debug('debug message')
-        self.lgr.info('info message')
-        self.lgr.warn('warn message')
-        self.lgr.error('error message')
-        self.lgr.critical('critical message')
-    def update_selected_database(self):
-        response = urllib.request.urlopen(
+        super().__init__()         
+        self.response = urllib.request.urlopen(
                    'http://siteresources.worldbank.org/INTPROSPECTS/Resources/' +\
                    'GemDataEXTR.zip')
-        zipfile_ = zipfile.ZipFile(io.BytesIO(response.read()))
-        excelfile = {name : zipfile_.read(name) for name in zipfile_.namelist()}
+        '''
+        ********Getting released date from headers of the Zipfile**********
+        '''           
+        self.releaseDates_ = self.response.getheaders()[1][1] 
+        self.releaseDates = [datetime.datetime.strptime(self.releaseDates_[5:], 
+        "%d %b %Y %H:%M:%S GMT")]
+        '''
+        *********************
+        '''
+       
+        self.zipfile_ = zipfile.ZipFile(io.BytesIO(self.response.read()))  
+        self.excelfile_ = {'GemDataEXTR':{name : self.zipfile_.read(name) for name in
+                           self.zipfile_.namelist()}}
+                           
+    def update_selected_database(self, datasetCode):
+        
+        if datasetCode == 'GEM':
+            excelfile = self.excelfile_['GemDataEXTR']
+        #excelfile = {name : self.zipfile_.read(name) for name in self.zipfile_.namelist()}
+        
+        '''
+        *********List of the name of the excel files**********
+        '''
+        concept_list=[]
+        [concept_list.append(key[:-5]) for key in excelfile.keys()]
+        '''
+        *********************
+        '''
+        
         for name_series in excelfile.keys():
             #Saving the Last modified date of each excel file
             index_name_series = list(excelfile.keys()).index(name_series)
-            released_dates = list(zipfile_.infolist()[index_name_series].date_time[0:6])
-            
+            last_Update = list(zipfile_.infolist()[index_name_series].date_time[0:6])
             excel_file = xlrd.open_workbook(file_contents = excelfile[name_series])
             for sheet_name in excel_file.sheet_names():
-                if sheet_name not in ['Sheet1','Sheet2','Sheet3','Sheet4','Feuille1','Feuille2','Feuille3','Feuille4']: 
+                if sheet_name not in ['Sheet1','Sheet2','Sheet3','Sheet4',
+                'Feuille1','Feuille2','Feuille3','Feuille4']: 
                     label_column_list = excel_file.sheet_by_name(sheet_name).col(0)[2:]
+                    
+                    '''
+                    ************List of countries or comodities***
+                    '''
+                    dimensionList=[]
+                    countries_list=[]
+                    [countries_list.append((excel_file.
+                    sheet_by_name(sheet_name).col(column_index))[0].value) for
+                    column_index in range (1, excel_file.sheet_by_name
+                    (sheet_name).ncols)]
+                    '''
+                    ****************
+                    '''
+                    dimensionList=[{'name':'concept', 'values': concept_list},
+                                   {'name':'country', 'values': countries_list}]
                     for column_index in range (1,
                         excel_file.sheet_by_name(sheet_name).ncols):
                         value = []
-                        column = excel_file.sheet_by_name(sheet_name).col(column_index)
+                        column = excel_file.sheet_by_name(sheet_name).\
+                        col(column_index)
                         if name_series[:-5] in ['Commodity Prices']:
                             dimensions_int = {'name':'Commodity Prices',
                             'value':column[0].value} 
@@ -75,23 +104,26 @@ class WorldBank(Skeleton):
                             frequency = 'd'
                         Key = name_series[:-5].replace(' ',
                                             '_').replace(',', '')+'.'+column[0].value
-                        document = Dataset(provider = 'WorldBank', name = name_series[:-5] , datasetCode = Key,lastUpdate = released_dates)
+                        document = Dataset(provider = 'WorldBank', 
+                                           name = name_series[:-5] ,
+                                           datasetCode = datasetCode, lastUpdate = last_Update,
+                                           dimensionList = dimensionList )
                         id = document.update_database()
+                        #print(dimensionList)
        
-    def categories_db(self):
-        response = urllib.request.urlopen(
-                   'http://siteresources.worldbank.org/INTPROSPECTS/Resources/' +\
-                   'GemDataEXTR.zip')
-        zipfile_ = zipfile.ZipFile(io.BytesIO(response.read()))
-        excelfile = {name : zipfile_.read(name) for name in zipfile_.namelist()}
+    def categories_db(self,datasetCode):
+        if datasetCode == 'GEM':
+            excelfile = self.excelfile_['GemDataEXTR']
+        #excelfile = {name : self.zipfile_.read(name) for name in self.zipfile_.namelist()}
         for name_series in excelfile.keys():
             #Saving the Last modified date of each excel file
             index_name_series = list(excelfile.keys()).index(name_series)
-            released_dates = str(zipfile_.infolist()[index_name_series].date_time[0:6])
+            last_Update = list(zipfile_.infolist()[index_name_series].date_time[0:6])
             
             excel_file = xlrd.open_workbook(file_contents = excelfile[name_series])
             for sheet_name in excel_file.sheet_names():
-                if sheet_name not in ['Sheet1','Sheet2','Sheet3','Sheet4','Feuille1','Feuille2','Feuille3','Feuille4']: 
+                if sheet_name not in ['Sheet1','Sheet2','Sheet3','Sheet4',
+                                    'Feuille1','Feuille2','Feuille3','Feuille4']: 
                     label_column_list = excel_file.sheet_by_name(sheet_name).col(0)[2:]
                     for column_index in range (1,
                         excel_file.sheet_by_name(sheet_name).ncols):
@@ -127,23 +159,26 @@ class WorldBank(Skeleton):
                         Key = name_series[:-5].replace(' ',
                                             '_').replace(',', '')+'.'+column[0].value
  
-                        document = Category(provider = 'WorldBank', name = name_series[:-5] , categoryCode = Key, lastUpdate = released_dates )
+                        document = Category(provider = 'WorldBank', 
+                                            name = name_series[:-5] , 
+                                            categoryCode = Key )
                         _id = document.update_database()
                                   
-    def update_a_series(self):
-        response = urllib.request.urlopen(
-                   'http://siteresources.worldbank.org/INTPROSPECTS/Resources/' +\
-                   'GemDataEXTR.zip')
-        zipfile_ = zipfile.ZipFile(io.BytesIO(response.read()))
-        excelfile = {name : zipfile_.read(name) for name in zipfile_.namelist()}
+    def update_a_series(self,datasetCode):
+        if datasetCode == 'GEM':
+            excelfile = self.excelfile_['GemDataEXTR']
+        #excelfile = {name : self.zipfile_.read(name) for name in self.zipfile_.namelist()}
         for name_series in excelfile.keys():
-            released_dates = []
+            #last_Update = []
             #Saving the Last modified date of each excel file
             index_name_series = list(excelfile.keys()).index(name_series)
-            released_dates.append(zipfile_.infolist()[index_name_series].date_time[0:6])
+            S = list(zipfile_.infolist()[index_name_series].date_time[0:3])
+            last_Update = [datetime.datetime(S[0],S[1],S[2])]
+            #last_Update.append(zipfile_.infolist()[index_name_series].date_time[0:6])
             excel_file = xlrd.open_workbook(file_contents = excelfile[name_series])
             for sheet_name in excel_file.sheet_names():
-                if sheet_name not in ['Sheet1','Sheet2','Sheet3','Sheet4','Feuille1','Feuille2','Feuille3','Feuille4']: 
+                if sheet_name not in ['Sheet1','Sheet2','Sheet3','Sheet4','Feuille1',
+                                      'Feuille2','Feuille3','Feuille4']: 
                     label_column_list = excel_file.sheet_by_name(sheet_name).col(0)[2:]
                     for column_index in range (1,
                         excel_file.sheet_by_name(sheet_name).ncols):
@@ -174,7 +209,15 @@ class WorldBank(Skeleton):
                             frequency = 'm'
                         if sheet_name == 'daily':    
                             frequency = 'd' 'day'                                             
-                        Key = name_series[:-5].replace(' ',
-                                            '_').replace(',', '')+'.'+column[0].value
-                        document = Series(provider = 'WorldBank', name = name_series[:-5] , key = Key, datasetCode = Key, values = value, period_index = pandas.period_range(start_date_b, end_date_b , freq = frequency) , releaseDates = released_dates ,frequency=frequency , dimensions =  dimensions_int)
-                        _id = document.update_database(key = Key)     
+                        series_key = name_series[:-5].replace(' ',
+                                            '_').replace(',', '')+'.'+\
+                                            column[0].value+ str(random.random())
+                        document = Series(provider = 'WorldBank', 
+                                          name = name_series[:-5] , key = series_key,
+                                          datasetCode = Key, values = value,
+                                          period_index = pandas.period_range
+                                          (start_date_b, end_date_b , freq = frequency)
+                                          , releaseDates = self.releaseDates ,
+                                          frequency=frequency , 
+                                          dimensions =  dimensions_int)
+                        _id = document.update_database(key=series_key)     
