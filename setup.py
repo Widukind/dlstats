f8c072fcfaf87bcae5602be45f4510872d0d129e
Michaël Malter <dev@michaelmalter.fr>
2015-04-08 23:00:36 +0200

    Handling exception when pip is not called from root

--- setup.py
+++ setup.py
@@ -27,11 +27,16 @@ setup(name='dlstats',
       ]
 	)
 
-with open('/etc/systemd/system/dlstats.service'):
-        os.chmod('/etc/systemd/system/dlstats.service', 0o755)
+try:
+    with open('/etc/systemd/system/dlstats.service'):
+            os.chmod('/etc/systemd/system/dlstats.service', 0o755)
 
-with open('/usr/local/bin/dlstats_server.py'):
-        os.chmod('/usr/local/bin/dlstats_server.py', 0o755)
+    with open('/usr/local/bin/dlstats_server.py'):
+            os.chmod('/usr/local/bin/dlstats_server.py', 0o755)
 
-with open('/etc/dlstats'):
-        os.chmod('/etc/dlstats', 0o755)
+    with open('/etc/dlstats'):
+            os.chmod('/etc/dlstats', 0o755)
+except PermissionError:
+    print("You should run the installation script as root if you want the
+          default configuration files")
+        
