b44ca68f3b218c41ac3c7c9999cc329cf28d8da1
Michaël Malter <dev@michaelmalter.fr>
2015-04-10 04:19:13 +0200

    Create in-memory database when mongo is unavailable
    This allows for easier testing.

--- dlstats/mongo_client.py
+++ dlstats/mongo_client.py
@@ -1,3 +1,11 @@
+import ming
 from pymongo import MongoClient
+from pymongo.errors import ConnectonFailure
 from . import configuration
-mongo_client = MongoClient(**configuration['MongoDB'])
+from dlstats import lgr
+try:
+    mongo_client = MongoClient(**configuration['MongoDB'])
+except ConnectonFailure: # Be careful. MongoClient don't throw that exception in the latest versions of pymongo
+    lgr.errors('Could not connect to MongoDB. Creating a database in RAM.')
+    datastore = ming.create_datastore('mim://')
+    mongo_client = datastore.conn
