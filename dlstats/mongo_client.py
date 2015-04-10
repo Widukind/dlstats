65495990e18cf2bf33e215970f2fd0f0288f8661
Michaël Malter <dev@michaelmalter.fr>
2015-04-10 12:59:59 +0200

    Typo

--- dlstats/mongo_client.py
+++ dlstats/mongo_client.py
@@ -1,11 +1,11 @@
 import ming
 from pymongo import MongoClient
-from pymongo.errors import ConnectonFailure
+from pymongo.errors import ConnectionFailure
 from . import configuration
 from dlstats import lgr
 try:
     mongo_client = MongoClient(**configuration['MongoDB'])
-except ConnectonFailure: # Be careful. MongoClient don't throw that exception in the latest versions of pymongo
+except ConnectionFailure: # Be careful. MongoClient don't throw that exception in the latest versions of pymongo
     lgr.errors('Could not connect to MongoDB. Creating a database in RAM.')
     datastore = ming.create_datastore('mim://')
     mongo_client = datastore.conn
