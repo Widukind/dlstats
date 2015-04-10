92e23b21b95731fecb03118d963cc1a2b6322970
Michaël Malter <dev@michaelmalter.fr>
2015-04-10 16:44:13 +0200

    Fix logging

--- dlstats/logger.py
+++ dlstats/logger.py
@@ -1,4 +1,18 @@
 import logging
+import logging.config
 
-logging.config.fileConfig('/etc/dlstats/logging.conf')
-logger = logging.getLogger('dlstats')
+config_filename = '/etc/dlstats/logging.conf'
+try:
+    file = os.open(config_filename)
+    logging.config.fileConfig(config_filename)
+    logger = logging.getLogger('dlstats')
+except:
+    logger = logging.getLogger('dlstats')
+    logger.setLevel(logging.DEBUG)
+    fh = logging.FileHandler('dlstats.log')
+    fh.setLevel(logging.DEBUG)
+    frmt = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(frmt)
+    logger.addHandler(fh)
+
--- dlstats/mongo_client.py
+++ dlstats/mongo_client.py
@@ -2,10 +2,10 @@ import ming
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 from . import configuration
-from dlstats import lgr
+from dlstats.logger import logger
 try:
     mongo_client = MongoClient(**configuration['MongoDB'])
 except ConnectionFailure: # Be careful. MongoClient don't throw that exception in the latest versions of pymongo
-    lgr.errors('Could not connect to MongoDB. Creating a database in RAM.')
+    logger.error('Could not connect to MongoDB. Creating a database in RAM.')
     datastore = ming.create_datastore('mim://')
     mongo_client = datastore.conn
