7383343f7fb77c74455a50490ad2886fcf36bbd5
Michaël Malter <dev@michaelmalter.fr>
2015-04-16 15:10:33 +0200

    Comment test for the moment

---
+++ dlstats/fetchers/test_ecb.py
@@ -0,0 +1,12 @@
+import unittest
+import mongomock
+import ulstats 
+from dlstats.fetchers._skeleton import (Skeleton, Category, Series, BulkSeries,
+                                      Dataset, Provider)
+import datetime
+from bson import ObjectId
+
+#class CategoriesTestCase(unittest.TestCase):
+
+#if __name__ == '__main__':
+#    unittest.main()
