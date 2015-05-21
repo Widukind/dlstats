842fe0c91b38ad95751a99d2df4077f35d7c942f
Michel Juillard <michel.juillard@mjui.fr>
2015-05-21 13:53:16 +0200

    fixing problem with ElasticSearch

--- dlstats/fetchers/world_bank.py
+++ dlstats/fetchers/world_bank.py
@@ -147,64 +147,8 @@ class WorldBank(Skeleton):
                                                 releaseDates= [self.releaseDates],
                                                 frequency=frequency,
                                                 dimensions=dimensions))
-        return(documents.bulk_update_database())
-        
-    def upsert_a_series(self,datasetCode):                              
-        if datasetCode == 'GEM':
-            excelfile = self.excelfile_['GemDataEXTR']
-        else:
-            raise Exception("The name of dataset was not entered!")
-        for name_series in excelfile.keys():
-            index_name_series = list(excelfile.keys()).index(name_series)
-            S = list(self.zipfile_.infolist()[index_name_series].date_time[0:3])
-            last_Update = [datetime.datetime(S[0],S[1],S[2])]
-            excel_file = xlrd.open_workbook(file_contents = excelfile[name_series])
-            for sheet_name in excel_file.sheet_names():
-                if sheet_name not in ['Sheet1','Sheet2','Sheet3','Sheet4','Feuille1',
-                                      'Feuille2','Feuille3','Feuille4']: 
-                    label_column_list = excel_file.sheet_by_name(sheet_name).col(0)[2:]
-                    for column_index in range (1,
-                        excel_file.sheet_by_name(sheet_name).ncols):
-                        value = []
-                        column = excel_file.sheet_by_name(sheet_name).col(column_index)
-                        dimensions_int = {}
-                        dimensions_int['concept'] = name_series[:-5]
-                        if name_series[:-5] == 'Commodity Prices':
-                            dimensions_int['Commodity Prices'] = column[0].value
-                        else:
-                            dimensions_int['country'] = column[0].value 
-                        column_value = column[1:-1]
-                        for cell_value in column_value :
-                            value.append(str(cell_value.value))
-                        if sheet_name in ('annual') :
-                            start_date_b = str(int(label_column_list[0].value))
-                            end_date_b = str(int(label_column_list[-1].value))
-                        if sheet_name not in ('annual') :       
-                            start_date = str(label_column_list[0].value)
-                            start_date_b = start_date.replace('M','-') 
-                            end_date = str(label_column_list[-1].value)
-                            end_date_b = end_date.replace('M','-')       
-                        if sheet_name == 'annual':    
-                            frequency = 'A'
-                        if sheet_name == 'quarterly':    
-                            frequency = 'q'
-                        if sheet_name == 'monthly':    
-                            frequency = 'm'
-                        if sheet_name == 'daily':    
-                            frequency = 'd' 'day'                                             
-                        series_key = name_series[:-5].replace(' ',
-                                            '_').replace(',', '')+'.'+\
-                                            column[0].value
-                        document = Series(provider = 'WorldBank', 
-                                          name = name_series[:-5]+'_'+column[0].value
-                                          , key = series_key,
-                                          datasetCode = 'GEM', values = value,
-                                          period_index = pandas.period_range
-                                          (start_date_b, end_date_b , freq = frequency)
-                                          , releaseDates = self.releaseDates ,
-                                          frequency=frequency , 
-                                          dimensions =  dimensions_int)
-                        _id = document.update_database(key=series_key)     
+        documents.bulk_update_database()
+        return( documents.bulk_update_elastic() )
 
 if __name__ == "__main__":
     import world_bank
