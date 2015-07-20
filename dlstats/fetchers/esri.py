79b10b3e1d507ba882919e6b7f01578fd29d3a92
Michaël Malter <dev@michaelmalter.fr>
2015-07-20 11:55:18 +0200

    Fixes formatting issues.
    It seems like spyder is doing a pretty poor job at formatting code. I
    think we could fix that by enabling a few options. It would be weird of
    spyder to be unable to do very basic things such as delete extraneous
    withespaces, manage indentation.

--- dlstats/fetchers/esri.py
+++ dlstats/fetchers/esri.py
@@ -33,25 +33,24 @@ class esri(Skeleton):
 #http://www.esri.cao.go.jp/jp/sna/data/data_list/sokuhou/files/2014/qe144_2/__icsFiles/afieldfile/2015/03/04/rdef-qk1442.csv
 
 #read the url links for Amount parts
-        self.url_amount = []        
-        url_list_amount = ['mg1442','mk1442','jg1442','jk1442','mfy1442','mcy1442','jfy1442','jcy1442'] 
+        self.url_amount = []
+        url_list_amount = ['mg1442','mk1442','jg1442','jk1442','mfy1442','mcy1442','jfy1442','jcy1442']
         for index in url_list_amount:
             self.url_amount.append('http://www.esri.cao.go.jp/jp/sna/data/data_list/sokuhou/files/2014/qe144_2/__icsFiles/afieldfile/2015/03/04/gaku-'+index+'.csv')
         #read the url links for deflator parts
         self.url_deflator = []
-        url_list_deflator = ['def-qg1442','def-qk1442','rdef-qg1442','rdef-qk1442'] 
+        url_list_deflator = ['def-qg1442','def-qk1442','rdef-qg1442','rdef-qk1442']
         for index in url_list_deflator :
             self.url_deflator.append('http://www.esri.cao.go.jp/jp/sna/data/data_list/sokuhou/files/2014/qe144_2/__icsFiles/afieldfile/2015/03/04/'+index+'.csv')
-        #        self.readers = []
-         #       self.releaseDates = []
-          #      self.files_ = {}
-           #     for self.url in self.urls:
-            #        self.response= urllib.request.urlopen(self.url)
-             #       self.readers.append(xlrd.open_workbook(file_contents = self.response.read()))
-                
+#            self.readers = []
+#            self.releaseDates = []
+#            self.files_ = {}
+#            for self.url in self.urls:
+#                self.response= urllib.request.urlopen(self.url)
+#                self.readers.append(xlrd.open_workbook(file_contents = self.response.read()))
         self.provider = Provider(name='esri', website='http://www.cao.go.jp/index-e.html/')
-        
-        
+
+
     def upsert_dataset(self, datasetCode):
         if datasetCode=='esri':
             url = self.url_amount[0]
@@ -63,18 +62,17 @@ class esri(Skeleton):
             for rrow in draft:
                 list_csv.append(rrow)
                 year_draft.append(list(rrow[i] for i in included_cols))
-            
+
             #Generating the year regarding to the standard format
             #year = year_draft[7:]
             #year_last = year[-4][0][:4]+'q4' 
-            #period_index = pandas.period_range(year[0][0][:4], year_last , freq = 'quarterly')                            
-
+            #period_index = pandas.period_range(year[0][0][:4], year_last , freq = 'quarterly')
             year = year_draft[7:]
             year_last = year[-4][0][:4]+'q4' 
-            period_index = pandas.period_range(year[0][0][:4], year_last , freq = 'quarterly')                            
+            period_index = pandas.period_range(year[0][0][:4], year_last , freq = 'quarterly')
             list_csv[5][0] = 'year'
-            
-            # flating tree structure
+
+            # flatens the tree structure
             for i, j in enumerate(list_csv[6]):
                 if j != '':
                     if list_csv[5][i] != '':
@@ -94,18 +92,18 @@ class esri(Skeleton):
                     
             datasetCode = 'esri'
             releaseDates =response.getheaders()[0][1] 
-            lastUpdate = datetime.datetime.strptime(releaseDates[5:], "%d %b %Y %H:%M:%S GMT")                             
-                
+            lastUpdate = datetime.datetime.strptime(releaseDates[5:], "%d %b %Y %H:%M:%S GMT")
+
             document = Dataset(provider = 'esri', 
                        name = year_draft[1][0] ,
                        datasetCode = 'esri', lastUpdate = lastUpdate,
                        dimensionList = dimensionList, 
                        docHref = "http://www.cao.go.jp/index-e.html") 
             #print(document)           
-            effective_dimension_list = self.update_series('esri', dimensionList)    
+            effective_dimension_list = self.update_series('esri', dimensionList)
             document.update_database()
             #print(effective_dimension_list)
-            effective_dimension_list = self.update_series('esri', dimensionList)    
+            effective_dimension_list = self.update_series('esri', dimensionList)
             document.update_database()
             document.update_es_database(effective_dimension_list)
         else:
@@ -126,12 +124,12 @@ class esri(Skeleton):
             #Generating the year regarding to the standard format
             year = year_draft[7:]
             year_last = year[-4][0][:4]+'q4' 
-            period_index = pandas.period_range(year[0][0][:4], year_last , freq = 'q')                            
+            period_index = pandas.period_range(year[0][0][:4], year_last , freq = 'q')
 
-            period_index = pandas.period_range(year[0][0][:4], year_last , freq = 'quarterly')                            
+            period_index = pandas.period_range(year[0][0][:4], year_last , freq = 'quarterly')
             list_csv[5][0] = 'year'
             
-            # flating tree structure
+            # flatening the tree structure
             for i, j in enumerate(list_csv[6]):
                 if j != '':
                     if list_csv[5][i] != '':
@@ -150,13 +148,13 @@ class esri(Skeleton):
             
             #dimensionList = {'content':dimensionList_content}
 
-            dimensionList_content = []  
+            dimensionList_content = []
             response = urllib.request.urlopen(url)
             reader = csv.DictReader(codecs.iterdecode(response, 'latin-1'), fieldnames=list_csv[5] ,delimiter=',')
             for i in range(len(reader.fieldnames)):
                 if reader.fieldnames[i]!='' and reader.fieldnames[i] != 'year'  :
                     dimensionList_content.append(reader.fieldnames[i])
-            
+
             dimensionList = {'content':dimensionList_content}
             values_columns = {}
             for f in reader.fieldnames:
@@ -190,9 +188,7 @@ class esri(Skeleton):
                                             releaseDates= [lastUpdate],
                                             frequency='q',
                                             dimensions=dimensions,
-                                            )) 
-                                            
-                                            
+                                            ))
                     pprint.pprint('provider=esri')
                     print(series_key)
                     print(series_name)
@@ -206,7 +202,7 @@ class esri(Skeleton):
             ff =    documents.bulk_update_elastic()  
             print(ff)                        
             return(documents.bulk_update_elastic())
-            
+
             for count in dimensionList:
                 dimensions = {}
                 series_name = count + '; ' + 'Japan'
