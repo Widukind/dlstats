e6795f7297ff443ca3c4e40393419b40b59e9b6e
Stéphane RAULT <stephane.rault@radicalspam.org>
2015-10-07 16:35:07 +0200

    fix bis LBS DISS URL

--- dlstats/constants.py
+++ dlstats/constants.py
@@ -1,5 +1,3 @@
 # -*- coding: utf-8 -*-
 
-BIS_URLS = [
-    "http://www.bis.org/statistics/full_bis_lbs_diss_csv.zip",            
-]
+BIS_LBS_DISS_URL = "http://www.bis.org/statistics/full_bis_lbs_diss_csv.zip"
