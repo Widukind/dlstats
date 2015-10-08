24d27c0ecb4758b09cb68d5319e8c541edca2134
Stéphane RAULT <stephane.rault@radicalspam.org>
2015-10-08 12:23:17 +0200

    add constants for mongodb collections names

--- dlstats/constants.py
+++ dlstats/constants.py
@@ -1,3 +1,18 @@
 # -*- coding: utf-8 -*-
 
 BIS_LBS_DISS_URL = "http://www.bis.org/statistics/full_bis_lbs_diss_csv.zip"
+
+COL_CATEGORIES = "categories"
+
+COL_PROVIDERS = "providers"
+
+COL_DATASETS = "datasets"
+
+COL_SERIES = "series"
+
+COL_ALL = [
+    COL_CATEGORIES, 
+    COL_PROVIDERS,
+    COL_DATASETS,
+    COL_SERIES
+]
