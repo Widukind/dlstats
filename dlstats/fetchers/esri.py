61ab0e2a91812cbb770fa25cfcbdd35f310b7022
Salimeh <salimeh@Salimeh-PC.lan.cepremap.org>
2015-10-22 17:09:55 +0200

    Adapt with new version

--- dlstats/fetchers/esri.py
+++ dlstats/fetchers/esri.py
@@ -1,38 +1,32 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr 24 09:53:55 2015
+Created on Fri Oct 16 10:59:20 2015
 
-
+@author: salimeh
 """
-from dlstats.fetchers._commons import Fetcher, Category, Series, BulkSeries, Dataset, Provider
+
+
+from dlstats.fetchers._commons import Fetcher, Category, Series, Dataset, Provider, CodeDict, ElasticIndex
 import urllib
-import codecs
-import datetime
-import pandas
+import xlrd
 import csv
-
+import codecs
+from datetime import datetime
+import pandas
 import pprint
+from collections import OrderedDict
+from re import match
+from time import sleep
+import requests
+from lxml import etree
 
 
-class esri(Fetcher):
+
+class Esri(Fetcher):
     def __init__(self):
-        super().__init__(provider_name='esri') 
-
-#Amount
-#http://www.esri.cao.go.jp/jp/sna/data/data_list/sokuhou/files/2014/qe144_2/__icsFiles/afieldfile/2015/03/04/gaku-mk1442.csv
-#http://www.esri.cao.go.jp/jp/sna/data/data_list/sokuhou/files/2014/qe144_2/__icsFiles/afieldfile/2015/03/04/gaku-jg1442.csv
-#http://www.esri.cao.go.jp/jp/sna/data/data_list/sokuhou/files/2014/qe144_2/__icsFiles/afieldfile/2015/03/04/gaku-jk1442.csv
-#http://www.esri.cao.go.jp/jp/sna/data/data_list/sokuhou/files/2014/qe144_2/__icsFiles/afieldfile/2015/03/04/gaku-mfy1442.csv
-#http://www.esri.cao.go.jp/jp/sna/data/data_list/sokuhou/files/2014/qe144_2/__icsFiles/afieldfile/2015/03/04/gaku-mcy1442.csv
-#http://www.esri.cao.go.jp/jp/sna/data/data_list/sokuhou/files/2014/qe144_2/__icsFiles/afieldfile/2015/03/04/gaku-jfy1442.csv
-#http://www.esri.cao.go.jp/jp/sna/data/data_list/sokuhou/files/2014/qe144_2/__icsFiles/afieldfile/2015/03/04/gaku-jcy1442.csv
-#Deflator
-#http://www.esri.cao.go.jp/jp/sna/data/data_list/sokuhou/files/2014/qe144_2/__icsFiles/afieldfile/2015/03/04/def-qg1442.csv
-#http://www.esri.cao.go.jp/jp/sna/data/data_list/sokuhou/files/2014/qe144_2/__icsFiles/afieldfile/2015/03/04/def-qk1442.csv
-#http://www.esri.cao.go.jp/jp/sna/data/data_list/sokuhou/files/2014/qe144_2/__icsFiles/afieldfile/2015/03/04/rdef-qg1442.csv
-#http://www.esri.cao.go.jp/jp/sna/data/data_list/sokuhou/files/2014/qe144_2/__icsFiles/afieldfile/2015/03/04/rdef-qk1442.csv
-
-#read the url links for Amount parts
+        super().__init__()         
+        self.provider_name = 'esri'
+        self.provider = Provider(name=self.provider_name,website='http://www.esri.cao.go.jp/index-e.html')
         self.url_amount = []
         url_list_amount = ['mg1442','mk1442','jg1442','jk1442','mfy1442','mcy1442','jfy1442','jcy1442']
         for index in url_list_amount:
@@ -42,188 +36,124 @@ class esri(Fetcher):
         url_list_deflator = ['def-qg1442','def-qk1442','rdef-qg1442','rdef-qk1442']
         for index in url_list_deflator :
             self.url_deflator.append('http://www.esri.cao.go.jp/jp/sna/data/data_list/sokuhou/files/2014/qe144_2/__icsFiles/afieldfile/2015/03/04/'+index+'.csv')
-#            self.readers = []
-#            self.releaseDates = []
-#            self.files_ = {}
-#            for self.url in self.urls:
-#                self.response= urllib.request.urlopen(self.url)
-#                self.readers.append(xlrd.open_workbook(file_contents = self.response.read()))
-        self.provider = Provider(name='esri', website='http://www.cao.go.jp/index-e.html/')
-
+        self.url_all = url_amount + url_deflator 
+        self.datasetCode_list = ['Nominal Gross Domestic Product (original series)',
+                'Nominal Gross Domestic Product (seasonally adjusted series)',
+                'Real Gross Domestic Product (original series)',
+                'Real Gross Domestic Product (seasonally adjusted series)',
+                'Annual Nominal GDP (fiscal year)',
+                'Annual Nominal GDP (calendar year)' ,
+                'Annual Real GDP (fiscal year)',
+                'Annual Real GDP (calendar year)',
+                'Deflators (quarter:original series)',
+                'Deflators (quarter:seasonally adjusted series)' ,
+                'Deflators (fiscal year)',
+                'Deflators (calendar year)']
+    def upsert_categories(self):
+        document = Category(provider = self.provider_name, 
+                            name = 'esri', 
+                            categoryCode ='esri',
+                            children = [None])
+        return document.update_database()
+        
+    def esri_issue(self):
+        for self.url in self.url_all :
+            datasetCode = self.datasetCode_list[self.url_all.index(self.url)]
+            self.upsert_dataset(datasetCode)
 
     def upsert_dataset(self, datasetCode):
-        if datasetCode=='esri':
-            url = self.url_amount[0]
-            response = urllib.request.urlopen(url)
-            draft = csv.reader(codecs.iterdecode(response, 'latin-1'), delimiter=',')
-            included_cols = [0]
-            list_csv = []
-            year_draft = []
-            for rrow in draft:
-                list_csv.append(rrow)
-                year_draft.append(list(rrow[i] for i in included_cols))
+        self.upsert_sna(self.url,datasetCode)                  
+        es = ElasticIndex()
+        es.make_index(self.provider_name,datasetCode)
 
-            #Generating the year regarding to the standard format
-            #year = year_draft[7:]
-            #year_last = year[-4][0][:4]+'q4' 
-            #period_index = pandas.period_range(year[0][0][:4], year_last , freq = 'quarterly')
-            year = year_draft[7:]
-            year_last = year[-4][0][:4]+'q4' 
-            period_index = pandas.period_range(year[0][0][:4], year_last , freq = 'quarterly')
-            list_csv[5][0] = 'year'
+    def upsert_sna(self, url, dataset_code):
+        dataset = Dataset(self.provider_name,dataset_code)
+        sna_data = EsriData(dataset,url)
+        dataset.name = dataset_code
+        dataset.doc_href = 'http://www.esri.cao.go.jp/index-e.html'
+        dataset.last_update = sna_data.releaseDate
+        dataset.series.data_iterator = sna_data
+        dataset.update_database()
 
-            # flatens the tree structure
-            for i, j in enumerate(list_csv[6]):
-                if j != '':
-                    if list_csv[5][i] != '':
-                        keep = list_csv[5][i]
-                        list_csv[5][i] = list_csv[5][i] + '_' + list_csv[6][i]          
-                    else :
-                        list_csv[5][i] = keep + '_' + list_csv[6][i]
-            
-            dimensionList_content = []  
-            response = urllib.request.urlopen(url)
-            reader = csv.DictReader(codecs.iterdecode(response, 'latin-1'), fieldnames=list_csv[5] ,delimiter=',')
-            for i in range(len(reader.fieldnames)):
-                if reader.fieldnames[i]!='' and reader.fieldnames[i] != 'year'  :
-                    dimensionList_content.append(reader.fieldnames[i])
-            
-            dimensionList = {'content':dimensionList_content}
-                    
-            datasetCode = 'esri'
-            releaseDates =response.getheaders()[0][1] 
-            lastUpdate = datetime.datetime.strptime(releaseDates[5:], "%d %b %Y %H:%M:%S GMT")
-
-            document = Dataset(provider = 'esri', 
-                       name = year_draft[1][0] ,
-                       datasetCode = 'esri', lastUpdate = lastUpdate,
-                       dimensionList = dimensionList, 
-                       docHref = "http://www.cao.go.jp/index-e.html") 
-            #print(document)           
-            effective_dimension_list = self.update_series('esri', dimensionList)
-            document.update_database()
-            #print(effective_dimension_list)
-            effective_dimension_list = self.update_series('esri', dimensionList)
-            document.update_database()
-            document.update_es_database(effective_dimension_list)
-        else:
-            raise Exception("The name of dataset was not entered!")        
-    def update_series(self,datasetCode,dimensionList):
-    
-        if datasetCode=='esri':
-            url = self.url_amount[0]
-            response = urllib.request.urlopen(url)
-            draft = csv.reader(codecs.iterdecode(response, 'latin-1'), delimiter=',')
-            included_cols = [0]
-            list_csv = []
-            year_draft = []
-            for rrow in draft:
-                list_csv.append(rrow)
-                year_draft.append(list(rrow[i] for i in included_cols))
-                
-            #Generating the year regarding to the standard format
-            year = year_draft[7:]
-            year_last = year[-4][0][:4]+'q4' 
-            period_index = pandas.period_range(year[0][0][:4], year_last , freq = 'q')
-
-            period_index = pandas.period_range(year[0][0][:4], year_last , freq = 'quarterly')
-            list_csv[5][0] = 'year'
-            
-            # flatening the tree structure
-            for i, j in enumerate(list_csv[6]):
-                if j != '':
-                    if list_csv[5][i] != '':
-                        keep = list_csv[5][i]
-                        list_csv[5][i] = list_csv[5][i] + '_' + list_csv[6][i]          
-                    else :
-                        list_csv[5][i] = keep + '_' + list_csv[6][i]
-            
-
-            #dimensionList_content = []  
-            #response = urllib.request.urlopen(url)
-            reader = csv.DictReader(codecs.iterdecode(response, 'latin-1'), fieldnames=list_csv[5] ,delimiter=',')
-            #for i in range(len(reader.fieldnames)):
-            #    if reader.fieldnames[i]!='' and reader.fieldnames[i] != 'year'  :
-            #        dimensionList_content.append(reader.fieldnames[i])
-            
-            #dimensionList = {'content':dimensionList_content}
-
-            dimensionList_content = []
-            response = urllib.request.urlopen(url)
-            reader = csv.DictReader(codecs.iterdecode(response, 'latin-1'), fieldnames=list_csv[5] ,delimiter=',')
-            for i in range(len(reader.fieldnames)):
-                if reader.fieldnames[i]!='' and reader.fieldnames[i] != 'year'  :
-                    dimensionList_content.append(reader.fieldnames[i])
-
-            dimensionList = {'content':dimensionList_content}
-            values_columns = {}
-            for f in reader.fieldnames:
-                if f :
-                    value_column=[]
-                    response = urllib.request.urlopen(url)
-                    reader = csv.DictReader(codecs.iterdecode(response, 'latin-1'), fieldnames=list_csv[5] ,delimiter=',')
-                    for rowdict in reader:
-                        value_column.append(rowdict[f])
-                        values_columns[f] = value_column[7:-1]                    
-            datasetCode = 'esri'
-            releaseDates =response.getheaders()[0][1] 
-            lastUpdate = datetime.datetime.strptime(releaseDates[5:], "%d %b %Y %H:%M:%S GMT") 
-            documents = BulkSeries(datasetCode,{})
-
-            for count_key in dimensionList:
-                for count in dimensionList[count_key]:
         
-                    dimensions = {}
-                    series_name = count + '; ' + 'Japan'
-                    series_key = 'esri.' + 'NationalAccount' + ';' + 'Japan' 
-                    value = values_columns[f]               
-                    dimensions['content'] = count
-                    #print('dooool')
-                    documents.append(Series(provider='esri',
-                                            key= series_key,
-                                            name=series_name,
-                                            datasetCode= 'esri',
-                                            period_index= period_index,
-                                            values=value,
-                                            releaseDates= [lastUpdate],
-                                            frequency='q',
-                                            dimensions=dimensions,
-                                            ))
-                    pprint.pprint('provider=esri')
-                    print(series_key)
-                    print(series_name)
-                    print( 'esri')
-                    print( period_index)
-                    print(value)
-                    print([lastUpdate])
-                    print('q')
-                    print(dimensions)
-            documents.bulk_update_database()  
-            ff =    documents.bulk_update_elastic()  
-            print(ff)                        
-            return(documents.bulk_update_elastic())
-
-            for count in dimensionList:
-                dimensions = {}
-                series_name = count + '; ' + 'Japan'
-                series_key = 'esri.' + 'NationalAccount' + ';' + 'Japan' 
-                value = values_columns[f]
-                dimensions['content'] = count
-                documents.append(Series(provider='esri',
-                                        key= series_key,
-                                        name=series_name,
-                                        datasetCode= 'esri',
-                                        period_index= period_index,
-                                        values=value,
-                                        releaseDates= [lastUpdate],
-                                        frequency='q',
-                                        dimensions=dimensions))                                                
-            return(documents.bulk_update_database())
-        else:
-            raise Exception("The name of dataset was not entered!")
+class EsriData():
+    def __init__(self,dataset,url):
+        self.provider_name = dataset.provider_name
+        self.dataset_code = dataset.dataset_code
+        self.dimension_list = dataset.dimension_list
+        self.attribute_list = dataset.attribute_list
+        self.panda_csv = pandas.read_csv(url)
+        response = urllib.request.urlopen(url)
+        releaseDate = response.info()['Last-Modified'] 
+        self.releaseDate = datetime.strptime(releaseDate, 
+                                                      "%a, %d %b %Y %H:%M:%S GMT")                                                  
+ 
+        if self.panda_csv.icol(0)[6] == '4' :
+            self.frequency = 'A'
+            ind = -1 
+        else :
+            self.frequency = 'Q'
+            ind = -4
+        end_date = self.panda_csv.icol(0)[len(self.panda_csv.icol(0))+ind][:4]
+        start_date = self.panda_csv.icol(0)[6][:4]
+        self.end_date = pandas.Period(end_date,freq = self.frequency).ordinal    
+        self.start_date = pandas.Period(start_date,freq = self.frequency).ordinal
+        self.column_range = iter(range(len(self.panda_csv.irow(5))))
+       #generating name of the series             
+        columns =self.panda_csv.columns
+        for column_ind in range(columns.size):
+            if str(self.panda_csv.icol(column_ind)[5]) != "nan":
+                self.panda_csv.icol(column_ind)[3] = str(self.panda_csv.icol(column_ind)[4])+'_'+str(self.panda_csv.icol(column_ind)[5])
+            else:    
+                self.panda_csv.icol(column_ind)[3] = str(self.panda_csv.icol(column_ind)[4])
+            if str(self.panda_csv.icol(column_ind)[4]) == "nan" :
+                if (str(self.panda_csv.icol(column_ind)[5]) != "nan") and (str(self.panda_csv.icol(column_ind-1)[4])) != "nan":         
+                    self.panda_csv.icol(column_ind)[3] = str(self.panda_csv.icol(column_ind-1)[4])+'_'+str(self.panda_csv.icol(column_ind)[5])
+                else:
+                    if str(self.panda_csv.icol(column_ind-1)[4]) == "nan":
+                        self.panda_csv.icol(column_ind)[3] = str(self.panda_csv.icol(column_ind-2)[4])+'_'+str(self.panda_csv.icol(column_ind)[5])            
+      
         
+        
+    def __next__(self):
+        column = self.panda_csv.icol(next(self.column_range))
+        if column is None:
+            raise StopIteration()
+        series = self.build_series(column)
+        if series is None:
+            raise StopIteration()            
+        return(series) 
+           
+                                           
+    def build_series(self,column):
+        
+        dimensions = {}
+        series = {}
+        series_value = [] 
+        
+        series_name = str(column[3]) + self.frequency 
+        series_key = 'esri.' + str(column[3]) + '; ' + self.frequency
+        dimensions['concept'] = self.dimension_list.update_entry('concept','',str(column[3]))  
+        
+        for r in range(6, len(column)):
+            series_value.append(str(column[r]))    
+        release_dates = [self.releaseDate for v in series_value] 
+        series['values'] = series_value                
+        series['provider'] = self.provider_name       
+        series['datasetCode'] = self.dataset_code
+        series['name'] = series_name
+        series['key'] = series_key
+        series['startDate'] = self.start_date
+        series['endDate'] = self.end_date  
+        series['releaseDates'] = release_dates
+        series['dimensions'] = dimensions
+        series['frequency'] = self.frequency
+        series['attributes'] = {}
+        return(series)
 
 if __name__ == "__main__":
-    import esri
-    w = esri.esri()
-    w.upsert_dataset('esri') 
+    e = Esri()
+    e.provider.update_database()
+    e.esri_issue()
+    e.upsert_categories()
+    
