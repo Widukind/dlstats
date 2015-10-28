a79b8b2f0dc48b1bfceeeab127e135c6d4020d7f
Stéphane RAULT <stephane.rault@radicalspam.org>
2015-10-28 07:05:07 +0100

    add version module

---
+++ dlstats/version.py
@@ -0,0 +1,5 @@
+
+VERSION = (0, 1, 0)
+
+def version_str():
+    return "%s.%s.%s" % VERSION
--- setup.py
+++ setup.py
@@ -3,8 +3,23 @@
 
 from setuptools import setup, find_packages
 
+def rq(s):
+    return s.strip("\"'")
+
+def version(filepath):
+	import re
+	import os
+	re_vers = re.compile(r'VERSION\s*=.*?\((.*?)\)')
+	here = os.path.abspath(os.path.dirname(__file__))
+	with open(os.path.join(here, filepath)) as fp:
+		for line in fp:
+			m = re_vers.match(line.strip())
+			if m:
+				v = list(map(rq, m.groups()[0].split(', ')))
+				return "{0}.{1}.{2}".format(*v[0:3])
+
 setup(name='dlstats',
-	version="0.1.0",
+	version=version('dlstats/version.py'),
     description='A python module that provides an interface between statistics providers and pandas.',
     author='Widukind team',
     author_email='dev@michaelmalter.fr',
