8434238826e1d3cb593c8274e914b84a33891749
Michaël Malter <dev@michaelmalter.fr>
2015-11-04 06:51:54 +0100

    Housekeeping

--- dlstats/__init__.py
+++ dlstats/__init__.py
@@ -1,10 +1,5 @@
 from .configuration import configuration
 from .elasticsearch_client import elasticsearch_client
-from .logger import logger
-import os
-
-if os.environ.get('DLSTATS_TEST_ENVIRONMENT') is None:
-    os.environ['DLSTATS_TEST_ENVIRONMENT'] = 'False'
-
 from .mongo_client import mongo_client
+from .logger import logger
 from . import fetchers, misc_func
