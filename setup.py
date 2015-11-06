00eb4bb56ee5fffa2142936b779adcd80ba8f803
Michaël Malter <dev@michaelmalter.fr>
2015-11-06 07:12:34 +0100

    pep8

--- setup.py
+++ setup.py
@@ -1,32 +1,34 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 from setuptools import setup, find_packages
+
 
 def rq(s):
     return s.strip("\"'")
 
+
 def version(filepath):
-	import re
-	import os
-	re_vers = re.compile(r'VERSION\s*=.*?\((.*?)\)')
-	here = os.path.abspath(os.path.dirname(__file__))
-	with open(os.path.join(here, filepath)) as fp:
-		for line in fp:
-			m = re_vers.match(line.strip())
-			if m:
-				v = list(map(rq, m.groups()[0].split(', ')))
-				return "{0}.{1}.{2}".format(*v[0:3])
+    import re
+    import os
+    re_vers = re.compile(r'VERSION\s*=.*?\((.*?)\)')
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, filepath)) as fp:
+        for line in fp:
+            m = re_vers.match(line.strip())
+            if m:
+                v = list(map(rq, m.groups()[0].split(', ')))
+                return "{0}.{1}.{2}".format(*v[0:3])
 
 setup(name='dlstats',
-	version=version('dlstats/version.py'),
-    description='A python module that provides an interface between statistics providers and pandas.',
-    author='Widukind team',
-    author_email='dev@michaelmalter.fr',
-    url='https://github.com/Widukind/dlstats', 
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
+      version=version('dlstats/version.py'),
+      description='A python module that provides an interface between\
+                   statistics providers and pandas.',
+      author='Widukind team',
+      author_email='dev@michaelmalter.fr',
+      url='https://github.com/Widukind/dlstats',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
         'requests>=2.4.3',
         'pymongo>=3.0.0',
         'pandas>=0.12',
@@ -38,19 +40,17 @@ setup(name='dlstats',
         'elasticsearch>=1.0.0,<2.0.0',
         'colorama>=0.3.3',
         'click>=5.1'
-    ],
-	entry_points={
-		'console_scripts': [
-			'dlstats = dlstats.client:main',
-		],
-	},			
-	tests_require=[
-		'nose>=1.0'
-		'coverage',
-		'flake8',
+      ],
+      entry_points={
+        'console_scripts': [
+          'dlstats = dlstats.client:main',
+        ],
+      },
+      tests_require=[
+        'nose>=1.0'
+        'coverage',
+        'flake8',
         'httpretty'
-	],
-	test_suite='nose.collector',
-)
-
-        
+      ],
+      test_suite='nose.collector',
+      )
