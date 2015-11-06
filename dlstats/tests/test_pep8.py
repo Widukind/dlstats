764041ab9c2f1bda71aef68e94bb955de94f9eb6
Michaël Malter <dev@michaelmalter.fr>
2015-11-06 07:32:46 +0100

    pep8

--- dlstats/tests/test_pep8.py
+++ dlstats/tests/test_pep8.py
@@ -5,13 +5,15 @@ import dlstats
 
 
 class TestPep8(unittest.TestCase):
-    """Run PEP8 on all files in this directory and subdirectories."""
+    """Run PEP8 on all modules in dlstats"""
+    @unittest.skipUnless('FULL_REMOTE_TEST' in os.environ,
+                         "Skip - not full remote test")
     def test_pep8(self):
         style = pep8.StyleGuide(quiet=True)
-        #style.options.ignore += ('',)
+        # style.options.ignore += ('',)
         dir = os.path.dirname(dlstats.__file__)
         style.input_dir(dir)
-        self.assertEqual(style.options.report.total_errors,0)
+        self.assertEqual(style.options.report.total_errors, 0)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     unittest.main()
