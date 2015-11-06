065a383b8773a1fbb5c985e492f82c330afd393a
Michaël Malter <dev@michaelmalter.fr>
2015-11-06 02:33:20 +0100

    Finalize test for upsert_categories

--- dlstats/tests/fetchers/test_ecb.py
+++ dlstats/tests/fetchers/test_ecb.py
@@ -40,13 +40,18 @@ class ECBCategoriesDBTestCase(BaseDBFetcherTestCase):
     def setUp(self):
         BaseDBFetcherTestCase.setUp(self)
         self.fetcher = ecb.ECB(db=self.db)
+    @patch('sdmx.ecb.dataflows',dataflows)
     @patch('dlstats.fetchers.ecb.ECB.get_categories',get_categories)
     def test_categories(self):
-        print(self.fetcher.upsert_categories())
-        results = self.db[constants.COL_CATEGORIES].find({"provider": self.fetcher.provider_name}, {'_id': False})
-        reference = pickle.loads(pkgutil.get_data('dlstats', 'tests/resources/ecb/ecb_categories_mongo_dict.pkl'))
-        self.assertEqual(results,reference)
+        #We exclude ObjectIDs because their values are not determinitstic. We
+        #can't test these elements
+        results = self.db[constants.COL_CATEGORIES].find(
+            {"provider": self.fetcher.provider_name},
+            {'_id': False, 'children': False})
+        reference = pickle.loads(pkgutil.get_data(
+            'dlstats', 'tests/resources/ecb/ecb_categories_mongo_dict.pkl'))
+        self.assertEqual([result for result in results],reference)
 
 if __name__ == '__main__':
-    #unittest.main()
-    print(generate_datasets_sdmx())
+    unittest.main()
+    #print(generate_datasets_sdmx())
