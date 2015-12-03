eda2f4d7a5aad33c4aefd7d05e096de826e8b1f2
Stéphane RAULT <stephane.rault@radicalspam.org>
2015-12-03 21:54:03 +0100

    add constants for normalize frequencies

--- dlstats/constants.py
+++ dlstats/constants.py
@@ -20,3 +20,37 @@ COL_ALL = [
     COL_DATASETS,
     COL_SERIES
 ]
+
+FREQ_ANNUALY = "A"
+FREQ_MONTHLY = "M"
+FREQ_QUATERLY = "Q"
+FREQ_WEEKLY = "W"
+FREQ_DAILY = "D"
+
+FREQUENCIES = (
+    (FREQ_ANNUALY, "Annualy"),
+    (FREQ_MONTHLY, "Monthly"),
+    (FREQ_QUATERLY, "Quaterly"),
+    (FREQ_WEEKLY, "Weekly"),
+    (FREQ_DAILY, "Daily"),
+)
+
+FREQUENCIES_DICT = dict(FREQUENCIES)
+
+FREQUENCIES_CONVERT = {
+    'Annualy': 'A',
+    'annualy': 'A',
+    'Monthly': 'M',
+    'monthly': 'M',
+    'Quaterly': 'Q',
+    'quaterly': 'Q',
+    'Weekly': 'W',
+    'weekly': 'W',
+    'Daily': 'D',
+    'daily': 'D',
+    'a': 'A',
+    'd': 'D',
+    'm': 'M',
+    'q': 'Q',
+    'w': 'W',
+}
