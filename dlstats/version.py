874be3c8fb960ee871d7ebcda76cb6049d6066eb
Michaël Malter <dev@michaelmalter.fr>
2015-12-03 11:53:54 +0100

    Handle versions without a dashed suffix

--- dlstats/version.py
+++ dlstats/version.py
@@ -2,4 +2,9 @@ VERSION = (0, 2, 0, 'dev')
 
 
 def version_str():
-    return "%s.%s.%s-%s" % VERSION
+    if len(VERSION) == 3: 
+        return "%s.%s.%s" % VERSION
+    elif len(VERSION) == 4: 
+        return "%s.%s.%s-%s" % VERSION
+    else:
+        raise IndexError("Incorrect format for the VERSION tuple")
