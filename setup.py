53c4964983282d544ef68199e2984d730db0210e
Stéphane RAULT <stephane.rault@radicalspam.org>
2015-12-18 08:44:34 +0100

    clean setup. use requires to requirements files

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 from setuptools import setup, find_packages
 
 from dlstats import version
@@ -9,33 +9,14 @@ setup(name='dlstats',
       description='A python module that provides an interface between\
                    statistics providers and pandas.',
       author='Widukind team',
-      author_email='dev@michaelmalter.fr',
       url='https://github.com/Widukind/dlstats',
       license='AGPLv3',
       packages=find_packages(),
       include_package_data=True,
-      install_requires=[
-        'requests>=2.4.3',
-        'pymongo>=3.0.0',
-        'pandas>=0.14.1',
-        'voluptuous>=0.8',
-        'xlrd>=0.8',
-        'beautifulsoup4>=4.4.0',
-        'lxml>=3.4.0',
-        'elasticsearch>=1.0.0,<2.0.0',
-        'colorama>=0.3.3',
-        'click>=5.1'
-      ],
       entry_points={
         'console_scripts': [
           'dlstats = dlstats.client:main',
         ],
       },
-      tests_require=[
-        'nose>=1.0',
-        'coverage',
-        'flake8',
-        'httpretty'
-      ],
       test_suite='nose.collector',
       )
