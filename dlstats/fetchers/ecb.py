5f6832693defc773ff59426dfc3540c39c5a274c
Stéphane RAULT <stephane.rault@radicalspam.org>
2016-01-08 05:43:12 +0100

    update ECB and add test

--- dlstats/fetchers/ecb.py
+++ dlstats/fetchers/ecb.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
+"""
+217 series / secondes
+"""
+
 import time
-import urllib
 from datetime import datetime
 import logging
 
@@ -10,7 +13,7 @@ import pandas
 
 from pandasdmx.api import Request
 
-from dlstats.fetchers._commons import Fetcher, Categories, Datasets, Providers
+from dlstats.fetchers._commons import Fetcher, Datasets, Providers
 from dlstats import constants
 from collections import OrderedDict
 
@@ -39,9 +42,10 @@ class ECB(Fetcher):
                                  fetcher=self)
         
         self.sdmx = sdmx or Request(agency=self.provider_name)
-        self.sdmx.set_timeout(90)
+        self.sdmx.timeout = 90
         
         self._dataflows = None
+        self._constraints = None
         self._categoryschemes = None
         self._categorisations = None
     
@@ -62,9 +66,8 @@ class ECB(Fetcher):
         self._categorisations = categorisation_response.msg.categorisations
         """
     
-        #http://sdw-wsrest.ecb.europa.eu/service/dataflow?references=all
-        #TODO: timeout !!!
-        dataflows_response = self.sdmx.get(resource_type='dataflow', agency=self.provider_name, params=dict(references=None))    
+        #http://sdw-wsrest.ecb.europa.eu/service/dataflow
+        dataflows_response = self.sdmx.get(resource_type='dataflow', agency=self.provider_name)    
         logger.debug(dataflows_response.url)
         self._dataflows = dataflows_response.msg.dataflows
 
@@ -115,12 +118,11 @@ class ECB(Fetcher):
                            last_update=datetime.now(),
                            fetcher=self)
         
-        dataset_doc = self.db[constants.COL_DATASETS].find_one({"provider_name": self.provider_name,
-                                                                "dataset_code": dataset_code})
+        #dataset_doc = self.db[constants.COL_DATASETS].find_one({"provider_name": self.provider_name,
+        #                                                        "dataset_code": dataset_code})
         
         insee_data = ECB_Data(dataset=dataset,
-                              dataset_doc=dataset_doc, 
-                              dataflow=dataflow, 
+                              #dataset_doc=dataset_doc, 
                               sdmx=self.sdmx)
         dataset.series.data_iterator = insee_data
         result = dataset.update_database()
@@ -135,46 +137,78 @@ class ECB(Fetcher):
 
 class ECB_Data(object):
     
-    def __init__(self, dataset=None, dataset_doc=None, dataflow=None, sdmx=None):
+    def __init__(self, dataset=None, 
+                 #dataset_doc=None, 
+                 sdmx=None):
         """
         :param Datasets dataset: Datasets instance
         :param pandasdmx.model.DataflowDefinition dataflow: instance of DataflowDefinition
         :param RequestINSEE sdmx: SDMX Client  
         """        
-        self.cpt = 0
         
         self.dataset = dataset
-        self.dataset_doc = dataset_doc
-        """
-        self.last_update = None
-        if self.dataset_doc:
-            self.last_update = self.dataset_doc["last_update"]
-        """
+        #self.dataset_doc = dataset_doc
         self.provider_name = self.dataset.provider_name
         self.dataset_code = self.dataset.dataset_code
-        self.dataflow = dataflow
         self.sdmx = sdmx
         
+        dataflows_response = self.sdmx.get(resource_type='dataflow', 
+                                           agency=self.provider_name, 
+                                           resource_id=self.dataset_code,
+                                           memcache='dataflow' + self.dataset_code)    
+        
+        logger.debug(dataflows_response.url)
+        
+        self.dataflow = dataflows_response.msg.dataflows[self.dataset_code] 
+
+        constraints = dataflows_response.msg.constraints
+        
+        constraints = [c for c in constraints.aslist() 
+                            if c.constraint_attachment.id == self.dataset_code]
+        
+        self.constraint = None
+        if len(constraints) == 1:
+            self.constraint = constraints[0]
+            
+        self.cube_region = None
+        if self.constraint:
+            self.cube_region = self.constraint.cube_region.key_values
+            
         self.dimension_list = self.dataset.dimension_list
         self.attribute_list = self.dataset.attribute_list
         
-        self.dsd_id = dataflow.structure.id
-        self.dsd = self.sdmx.get(resource_type='datastructure', 
+        self.dsd_id = self.dataflow.structure.id
+        self.datastructure = self.sdmx.get(resource_type='datastructure', 
                                  resource_id=self.dsd_id,
                                  agency=self.dataset.provider_name,
                                  headers={"Accept-Encoding": "gzip, deflate"},
-                                 params=dict(references='all')).msg.datastructures[self.dsd_id]
-
+                                 params=dict(references='all'))
+        
+        #TODO: voir si necessaire avec dataflow + reference ???
+        
+        self.dsd = self.datastructure.msg.datastructures[self.dsd_id]
+        
         self.dimensions = OrderedDict([(dim.id, dim) for dim in self.dsd.dimensions.aslist() if dim.id not in ['TIME', 'TIME_PERIOD']])
         self.dim_keys = list(self.dimensions.keys())
         self.dimension_list.set_dict(self.dimensions_to_dict())
         
-        '''Selection de la dimension avec le moins de variantes'''
-        self.dim_select = self.select_short_dimension()
+        '''Selection de la dimension avec le plus de variantes'''
+        self.dim_select_key = self.select_dimension_split()
+        self.dim_select = self.dimensions[self.dim_select_key]
+        self.dim_select_values = None
         
-        self.current_series = {}
+        if self.cube_region and self.dim_select_key in self.cube_region:
+            self.dim_select_values = self.cube_region[self.dim_select_key]
+        else:
+            self.dim_select_values = self.dim_select.local_repr.enum.keys()
+
+        print("self.dim_select_key : ", self.dim_select_key)
+        print("self.dim_select_values : ", self.dim_select_values)
         
         self.rows = self.get_series(self.dataset_code)
+        
+    def get_dim_values_for_select(self):
+        pass
         
     def dimensions_to_dict(self):
         _dimensions = {}
@@ -193,26 +227,40 @@ class ECB_Data(object):
     
     def concept_name(self, concept_id):
         #TODO: pas normal, l'information devrait être dans la self.dsd
-        return self.datastructure.msg.conceptschemes.aslist()[0][concept_id].name.en
+        return self.datastructure.conceptschemes.aslist()[0][concept_id].name.en
         
-    def select_short_dimension(self):
-        """Renvoi le nom de la dimension qui contiens le moins de valeur
+    def select_dimension_split(self):
+        """Renvoi le nom de la dimension qui contiens le plus de valeur
         pour servir ensuite de filtre dans le chargement des données (...)
+        
         """
-        _dimensions = {}        
+        _dimensions = {}
         
-        for dim_id, dim in self.dimensions.items():
-            _dimensions[dim_id] = len(dim.local_repr.enum.keys())
+        if self.cube_region:
+            for dim_id, values in self.cube_region.items():
+                _dimensions[len(values)] = dim_id
+        else:
+            for dim_id, dim in self.dimensions.items():
+                _dimensions[len(dim.local_repr.enum.keys())] = dim_id
         
-        _key = min(_dimensions, key=_dimensions.get)
-        return self.dimensions[_key]
+        select_value = max(_dimensions.keys())
+        return _dimensions[select_value] 
 
     def is_valid_frequency(self, series):
         """
-        http://www.bdm.insee.fr/series/sdmx/codelist/FR1/CL_FREQ
-        S: Semestrielle
-        B: Bimestrielle
-        I: Irregular
+        Pb Frequency ECB:
+        https://sdw-wsrest.ecb.europa.eu/service/dataflow/ECB/EXR?references=all
+        https://sdw-wsrest.ecb.europa.eu/service/data/EXR/.ARS...
+        A: Annual (2015)
+        B: Business (Pas d'exemple)
+        D: Daily (2000-01-13)
+        E: Event (not supported)
+        H: Half-yearly (2000-S2)
+        M: Monthly (2000-02)
+        N: Minutely (Pas d'exemple)
+        Q: Quarterly (2000-Q2)
+        S: Half Yearly, semester (value H exists but change to S in 2009, move from H to this new value to be agreed in ESCB context) (Pas d'exemple)
+        W: Weekly (Pas d'exemple)
         """
         frequency = None
         valid = True
@@ -224,16 +272,24 @@ class ECB_Data(object):
         
         if not frequency:
             valid = False        
-        elif frequency in ["S", "B", "I"]:
+        elif not frequency in ["A", "D", "M", "Q", "W"]: #TODO: N (minute) ?
             valid = False
         
         if not valid:
-            logger.warning("Not valid frequency[%s] - dataset[%s] - idbank[%s]" % (frequency, self.dataset_code, series.attrib.IDBANK))
+            logger.warning("Not valid frequency[%s] - dataset[%s] - key[%s]" % (frequency, self.dataset_code, self.get_series_key(series)))
         
         return valid
     
     def is_updated(self, series):
         """Verify if series changes
+        
+        TODO: voir headers result
+        
+        'Last-Modified': 'Fri, 08 Jan 2016 04:35:48 GMT'
+        
+        REQUEST HEADERS  {'Accept': '*/*', 'Accept-Encoding': 'gzip, deflate', 'Connection': 'keep-alive', 'User-Agent': 'python-requests/2.9.1'}
+        2016-01-08 05:35:42 dlstats.fetchers.ecb: [INFO] - http://sdw-wsrest.ecb.int/service/data/SST/..VPC....
+        2016-01-08 05:35:42 dlstats.fetchers.ecb: [INFO] - {'Cache-Control': 'max-age=0, no-cache, no-store', 'Connection': 'keep-alive, Transfer-Encoding', 'Content-Encoding': 'gzip', 'Expires': 'Fri, 08 Jan 2016 04:35:49 GMT', 'Server': 'Apache-Coyote/1.1', 'Pragma': 'no-cache', 'Vary': 'Accept, Accept-Encoding', 'Date': 'Fri, 08 Jan 2016 04:35:49 GMT', 'Transfer-Encoding': 'chunked', 'Content-Type': 'application/xml', 'Last-Modified': 'Fri, 08 Jan 2016 04:35:48 GMT'}        
         
         > dans la requete: 2009-05-15T14:15:00+01:00
         updatedAfter=2009-05-15T14%3A15%3A00%2B01%3A00
@@ -247,72 +303,41 @@ class ECB_Data(object):
         
         >>> str(datetime.now())
         '2015-12-26 08:21:15.987529'
-        
-        
         """
-        if not self.last_update:
-            return True
-        
-        _is_updated = datetime.strptime(series.attrib.LAST_UPDATE, "%Y-%m-%d") > self.last_update
-
-        if not _is_updated and logger.isEnabledFor(logging.INFO):
-            logger.info("bypass series updated dataset_code[%s] - idbank[%s]" % (self.dataset_code, series.attrib.IDBANK))
-        
-        return _is_updated
+        raise NotImplementedError()
 
     def get_series(self, dataset_code):
         """Load series for current dataset
-
-        #TODO: traduire:
-        
-        1er appel: sans filtrage sur les dimensions
-        
-        Appel suivant seulement si le 1er appel échoue avec un code HTTP_ERROR_LONG_RESPONSE
-        
-        Boucle sur les valeurs de le plus petite dimensions pour charger en plusieurs fois les series du dataset
-        
         """
 
-        def _request(key=''):
+        def _request(key):
             return self.sdmx.get(resource_type='data', 
                                  resource_id=dataset_code,
                                  agency=self.dataset.provider_name,
                                  key=key,
-                                 headers={"Accept-Encoding": "gzip, deflate"}, 
+                                 #headers={"Accept-Encoding": "gzip, deflate"}, 
                                  )
             
         try:
-            ''' First call - all series '''
-            _data = _request()
-            for s in _data.msg.data.series:
-                if self.is_valid_frequency(s):#TODO: and self.is_updated(s):
-                    yield s                        
-        except requests.exceptions.HTTPError as err:
-
-            if err.response.status_code == HTTP_ERROR_LONG_RESPONSE:
-                ''' Next calls if first call fail '''                
-                codes = list(self.dim_select.local_repr.enum.keys())
-                dim_select_id = str(self.dim_select.id)
-                
-                for code in codes:
-                    key = {dim_select_id: code}
-                    try:
-                        _data = _request(key)
-                        for s in _data.msg.data.series:
-                            if self.is_valid_frequency(s):#TODO: and self.is_updated(s):
-                                yield s
-                    except requests.exceptions.HTTPError as err:
-                        if err.response.status_code == HTTP_ERROR_NO_RESULT:
-                            continue
+            codes = self.dim_select_values
+            
+            for code in codes:
+                key = {self.dim_select_key: code}
+                try:
+                    _data = _request(key)
+                    logger.info(_data.url)
+                    logger.info(_data.http_headers)
+                    for s in _data.msg.data.series:
+                        if self.is_valid_frequency(s):
+                            yield s
                         else:
-                            raise
-
-            elif err.response.status_code == HTTP_ERROR_NO_RESULT:
-                ''' Not error - this series are not datas '''
-                raise ContinueRequest("No result")
-            else:
-                raise
-                
+                            yield
+                except requests.exceptions.HTTPError as err:
+                    if err.response.status_code == HTTP_ERROR_NO_RESULT:
+                        continue
+                    else:
+                        raise
+                                            
         except Exception as err:
             logger.error(str(err))
             raise
@@ -328,28 +353,30 @@ class ECB_Data(object):
         bson = self.build_series(_series)
         return bson
 
+    def get_series_key(self, series):
+        return ".".join(series.key._asdict().values())
+
     def get_series_name(self, series):
         if 'TITLE_COMPL' in series.attrib._fields:
             return series.attrib.TITLE_COMPL
         elif 'TITLE' in series.attrib._fields:
             return series.attrib.TITLE
         else:
+            #FIXME: Non ?
             return "-".join(series.key._asdict().values())
-        
-    def build_series(self, series):
-        """
-        :param series: Instance of pandasdmx.model.Series
-        
-        Serie Name     M-U2-N-V-L30-A-1-U2-2300-Z01-E
-        Serie Key     M.U2.N.V.L30.A.1.U2.2300.Z01.E        
-        """
-        bson = {}
-        bson['provider_name'] = self.provider_name
-        bson['dataset_code'] = self.dataset_code
-        bson['key'] = ".".join(series.key._asdict().values())
-        bson['name'] = self.get_series_name(series)
-        bson['last_update'] = self.dataset.last_update
-                
+    
+    def get_series_frequency(self, series):
+        if 'FREQ' in series.key._fields:
+            return series.key.FREQ
+        elif 'FREQ' in series.attrib._fields:
+            return series.attrib.FREQ
+        else:
+            raise Exception("Not FREQ field in series.key or series.attrib")
+    
+    def get_last_update(self, series):
+        return self.dataset.last_update
+    
+    def debug_series(self, series, bson):
         if logger.isEnabledFor(logging.DEBUG):
             import json
             try:
@@ -358,28 +385,39 @@ class ECB_Data(object):
                     "key": bson['key'],
                     "last_update": str(self.dataset.last_update),
                     "dimensions_keys": self.dim_keys,
-                    "series.attrib": series.attrib._asdict().items(),
-                    "series.key": series.key._asdict().items(),
+                    "series.attrib": list(series.attrib._asdict().items()),
+                    "series.key": list(series.key._asdict().items()),
                 }            
                 logger.debug(json.dumps(_debug))
-            except:
-                pass
-
-        if 'FREQ' in series.key._fields:
-            bson['frequency'] = series.key.FREQ
-        elif 'FREQ' in series.attrib._fields:
-            bson['frequency'] = series.attrib.FREQ
-        else:
-            raise Exception("Not FREQ field in series.key or series.attrib")
-        
+            except Exception as err:
+                print("JSON EXCEPTION : ", err)
+    
+    def get_attributes(self, series):
         attributes = {}
+        
+        #TODO: self.attribute_list         
         for obs in series.obs(with_values=False, with_attributes=True, reverse_obs=False):
             for key, value in obs.attrib._asdict().items():
                 if not key in attributes:
                     attributes[key] = []
                 attributes[key].append(value)
+        return attributes
+    
+    def build_series(self, series):
+        """
+        :param series: Instance of pandasdmx.model.Series
+        """
+        bson = {}
+        bson['provider_name'] = self.provider_name
+        bson['dataset_code'] = self.dataset_code
+        bson['key'] = self.get_series_key(series)
+        bson['name'] = self.get_series_name(series)
+        bson['last_update'] = self.get_last_update(series)
         
-        bson['attributes'] = attributes
+        self.debug_series(series, bson)
+        
+        bson['frequency'] = self.get_series_frequency(series)
+        bson['attributes'] = self.get_attributes(series)
             
         dimensions = dict(series.key._asdict())
         
@@ -387,14 +425,14 @@ class ECB_Data(object):
             if d in ['TITLE', 'TITLE_COMPL']:
                 continue
             dim_short_id = value
-            dim_long_id = self.concept_name(d)            
+            dim_long_id = self.concept_name(d)
             dimensions[d] = self.dimension_list.update_entry(d, dim_short_id, dim_long_id)
 
         bson['dimensions'] = dimensions
         
         _dates = [o.dim for o in series.obs(with_values=False, with_attributes=False, reverse_obs=False)]
-        bson['start_date'] = pandas.Period(_dates[-1], freq=bson['frequency']).ordinal
-        bson['end_date'] = pandas.Period(_dates[0], freq=bson['frequency']).ordinal
+        bson['start_date'] = pandas.Period(_dates[0], freq=bson['frequency']).ordinal
+        bson['end_date'] = pandas.Period(_dates[-1], freq=bson['frequency']).ordinal
         
         bson['values'] = [str(o.value) for o in series.obs(with_values=True, with_attributes=False, reverse_obs=False)]
 
