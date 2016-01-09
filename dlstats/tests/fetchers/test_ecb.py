d837cf6a44e96b17cbfa266f52b0af137924fae9
Stéphane RAULT <stephane.rault@radicalspam.org>
2016-01-09 11:33:14 +0100

    update ecb tests

--- dlstats/tests/fetchers/test_ecb.py
+++ dlstats/tests/fetchers/test_ecb.py
@@ -7,7 +7,7 @@ from pprint import pprint
 import pandas
 
 from dlstats.fetchers._commons import Datasets
-from dlstats.fetchers.ecb import ECB, ECB_Data, ContinueRequest
+from dlstats.fetchers.ecb import ECB as Fetcher, ECB_Data as FetcherData, ContinueRequest
 from dlstats import constants
 
 import unittest
@@ -18,31 +18,27 @@ from dlstats.tests.base import RESOURCES_DIR, BaseDBTestCase
 
 """
 Load files with httpie tools:
-    http http://www.bdm.insee.fr/series/sdmx/dataflow references==all Accept:application/xml Content-Type:application/xml > insee-dataflow.xml
-    http http://www.bdm.insee.fr/series/sdmx/datastructure/FR1/IPI-2010-A21 references==all Accept:application/xml Content-Type:application/xml > insee-IPI-2010-A21-datastructure.xml
-    http http://sdw-wsrest.ecb.int/service/data/EXR/M.NOK.EUR.SP00.A > ecb-M.NOK.EUR.SP00.A-data.xml
-    
-    Accept:application/vnd.sdmx.genericdata+xml;version=2.1 
-    
-    http http://www.bdm.insee.fr/series/sdmx/datastructure/FR1/CNA-2010-CONSO-SI-A17 references==all Accept:application/xml Content-Type:application/xml > insee-bug-data-namedtuple-datastructure.xml
-    
+    http "http://sdw-wsrest.ecb.int/service/dataflow/ECB" > ecb-dataflow.xml
+    http "http://sdw-wsrest.ecb.europa.eu/service/dataflow/ECB/EXR?references=all" > ecb-EXR-dataflow.xml
+    http "http://sdw-wsrest.ecb.europa.eu/service/datastructure/ECB/ECB_EXR1?references=all" > ecb-ECB_EXR1-datastructure.xml
+    http "http://sdw-wsrest.ecb.int/service/data/EXR/M.NOK.EUR.SP00.A" > ecb-data-M.NOK.EUR.SP00.A.xml
+    http "http://sdw-wsrest.ecb.int/service/data/EXR/.ARS+AUD.EUR.SP00.A" > ecb-data-X.ARS+AUD.NOK.EUR.SP00.A.xml
 """
-DATAFLOW_FP = os.path.abspath(os.path.join(RESOURCES_DIR, "insee-dataflow.xml"))
+DATAFLOW_FP = os.path.abspath(os.path.join(RESOURCES_DIR, "ecb-dataflow.xml"))
+
+DATAFLOW_COUNT = 56
 
 DATASETS = {
-    'IPI-2010-A21': {
-        'data-fp': os.path.abspath(os.path.join(RESOURCES_DIR, "insee-IPI-2010-A21-data.xml")),
-        'datastructure-fp': os.path.abspath(os.path.join(RESOURCES_DIR, "insee-IPI-2010-A21-datastructure.xml")),
-        'series_count': 20,
-    },
-    'CNA-2010-CONSO-SI-A17': {
-        'data-fp': os.path.abspath(os.path.join(RESOURCES_DIR, "insee-bug-data-namedtuple.xml")),
-        'datastructure-fp': os.path.abspath(os.path.join(RESOURCES_DIR, "insee-bug-data-namedtuple-datastructure.xml")),
-        'series_count': 1,
+    'EXR': {
+        'dataflow-fp': os.path.abspath(os.path.join(RESOURCES_DIR, "ecb-EXR-dataflow.xml")),
+        'data-fp': os.path.abspath(os.path.join(RESOURCES_DIR, "ecb-data-M.NOK.EUR.SP00.A.xml")),
+        #'data-fp': os.path.abspath(os.path.join(RESOURCES_DIR, "ecb-data-X.ARS+AUD.NOK.EUR.SP00.A.xml")),        
+        'datastructure-fp': os.path.abspath(os.path.join(RESOURCES_DIR, "ecb-ECB_EXR1-datastructure.xml")),
+        'series_count': 1
     },
 }
 
-class Mock_Data(ECB_Data):
+class Mock_Data(FetcherData):
     
     def __init__(self, **kwargs):
         self._series = []
@@ -60,6 +56,10 @@ class Mock_Data(ECB_Data):
         self._series.append(bson)
         return bson
     
+    def get_dim_select(self):
+        #return [None]
+        return [{"FREQ": "M"}]
+    
 def mock_upsert_dataset(self, dataset_code):
 
     self.load_structure(force=False)
@@ -76,79 +76,126 @@ def mock_upsert_dataset(self, dataset_code):
                        last_update=datetime(2015, 12, 24),
                        fetcher=self)
     
-    query = {'provider_name': self.provider_name, "dataset_code": dataset_code}
-    dataset_doc = self.db[constants.COL_DATASETS].find_one(query)
-    
-    self.insee_data = Mock_Data(dataset=dataset,
-                                dataset_doc=dataset_doc, 
-                                sdmx=self.sdmx)
-    dataset.series.data_iterator = self.insee_data
+    self._data = Mock_Data(dataset=dataset)
+    dataset.series.data_iterator = self._data
     result = dataset.update_database()
 
     return result
 
 
-class InseeTestCase(BaseDBTestCase):
+class FetcherTestCase(BaseDBTestCase):
     
-    # nosetests -s -v dlstats.tests.fetchers.test_insee:InseeTestCase
+    # nosetests -s -v dlstats.tests.fetchers.test_ecb:FetcherTestCase
     
     def setUp(self):
         BaseDBTestCase.setUp(self)
-        self.insee = INSEE(db=self.db)
+        self.fetcher = Fetcher(db=self.db)
         
     def _load_dataset(self, dataset_code):
 
+        #TODO: use tests.utils
         def _body(filepath):
             '''body for large file'''
             with open(filepath, 'rb') as fp:
                 for line in fp:
                     yield line        
         
-        url_dataflow = "http://www.bdm.insee.fr/series/sdmx/dataflow/INSEE"
+        #http://sdw-wsrest.ecb.int/service/dataflow/ECB
+        url_dataflow = "http://sdw-wsrest.ecb.int/service/dataflow/ECB"
         httpretty.register_uri(httpretty.GET, 
                                url_dataflow,
                                body=_body(DATAFLOW_FP),
+                               #match_querystring=True,
+                               status=200,
+                               streaming=True,
+                               content_type='application/vnd.sdmx.structure+xml;version=2.1')
+
+        url_dataflow_for_dataset = "http://sdw-wsrest.ecb.int/service/dataflow/ECB/EXR?references=all"
+        httpretty.register_uri(httpretty.GET, 
+                               url_dataflow_for_dataset,
+                               body=_body(DATASETS[dataset_code]['dataflow-fp']),
                                match_querystring=True,
                                status=200,
                                streaming=True,
-                               content_type="application/xml")
+                               content_type='application/vnd.sdmx.structure+xml;version=2.1')
         
-        self.insee._dataflows = self.insee.sdmx.get(resource_type='dataflow').msg.dataflows
-        
-        url_datastructure = "http://www.bdm.insee.fr/series/sdmx/datastructure/INSEE/%s" % dataset_code
+        url_datastructure = "http://sdw-wsrest.ecb.int/service/datastructure/ECB/ECB_EXR1?references=all"# % dataset_code
         httpretty.register_uri(httpretty.GET, 
                                url_datastructure,
                                body=_body(DATASETS[dataset_code]['datastructure-fp']),
-                               streaming=True,
+                               match_querystring=True,
                                status=200,
-                               content_type="application/xml")
+                               streaming=True,
+                               content_type='application/vnd.sdmx.structure+xml;version=2.1')
 
-        url_data = "http://www.bdm.insee.fr/series/sdmx/data/%s" % dataset_code
+        def request_callback(request, uri, headers):
+            #print("request : ", request)
+            #print("uri : ", uri)
+            #print("headers : ", headers)
+            #uri :  http://sdw-wsrest.ecb.int/service/data/EXR/M....
+            return (200, {"Content-Type": 'application/vnd.sdmx.genericdata+xml;version=2.1'}, _body(DATASETS[dataset_code]['data-fp']))
+    
+        #http://sdw-wsrest.ecb.int/service/data/EXR/A.ARS...
+        #http://sdw-wsrest.ecb.int/service/data/EXR/M.NOK.EUR.SP00.A
+        url_data = "http://sdw-wsrest.ecb.int/service/data/EXR/M...." #% dataset_code
         httpretty.register_uri(httpretty.GET, 
                                url_data,
-                               body=_body(DATASETS[dataset_code]['data-fp']),
-                               streaming=True,
+                               body=_body(DATASETS[dataset_code]['data-fp']), #request_callback, 
+                               match_querystring=True,
                                status=200,
-                               content_type="application/xml")
+                               streaming=True,
+                               content_type='application/vnd.sdmx.genericdata+xml;version=2.1')
+
+        url_data = "http://sdw-wsrest.ecb.int/service/data/EXR"
+        httpretty.register_uri(httpretty.GET, 
+                               url_data,
+                               body=_body(DATASETS[dataset_code]['data-fp']), 
+                               match_querystring=True,
+                               status=200,
+                               streaming=True,
+                               content_type='application/vnd.sdmx.genericdata+xml;version=2.1')
+
+    @httpretty.activate     
+    def test_headers(self):
+
+        # nosetests -s -v dlstats.tests.fetchers.test_ecb:FetcherTestCase.test_headers
+
+        dataset_code = 'EXR'
         
+        self._load_dataset(dataset_code)
+
+        response = self.fetcher.sdmx.get(resource_type='dataflow')
+        self.assertEqual(response.http_headers['server'], 'Python/HTTPretty')
+        self.assertEqual(response.url, 'http://sdw-wsrest.ecb.int/service/dataflow/ECB')
+        self.assertEqual(response.http_headers['content-type'], 'application/vnd.sdmx.structure+xml;version=2.1')
+        
+        response = self.fetcher.sdmx.get(resource_type='data', 
+                                 resource_id=dataset_code,
+                                 key={"FREQ": "M"})        
+        self.assertEqual(response.http_headers['server'], 'Python/HTTPretty')
+        self.assertEqual(response.url, 'http://sdw-wsrest.ecb.int/service/data/EXR/M....')
+        self.assertEqual(response.http_headers['content-type'], 'application/vnd.sdmx.genericdata+xml;version=2.1')
    
     @httpretty.activate     
+    @mock.patch('dlstats.fetchers.ecb.ECB.upsert_dataset', mock_upsert_dataset)    
     def test_load_dataset(self):
         
-        # nosetests -s -v dlstats.tests.fetchers.test_insee:InseeTestCase.test_load_dataset
+        # nosetests -s -v dlstats.tests.fetchers.test_ecb:FetcherTestCase.test_load_dataset
         
-        dataset_code = 'IPI-2010-A21'
+        dataset_code = 'EXR'
         
         self._load_dataset(dataset_code)
         
-        self.assertEqual(len(self.insee._dataflows.keys()), 663)
+        self.fetcher.load_structure()
         
-        self.assertTrue(dataset_code in self.insee._dataflows)
+        self.assertEqual(len(self.fetcher._dataflows.keys()), DATAFLOW_COUNT)
+        
+        self.assertTrue(dataset_code in self.fetcher._dataflows)
 
-        result = self.insee.upsert_dataset(dataset_code)
+        result = self.fetcher.upsert_dataset(dataset_code)
         
         query = {
-            'provider_name': self.insee.provider_name,
+            'provider_name': self.fetcher.provider_name,
             "dataset_code": dataset_code
         }
 
@@ -157,46 +204,40 @@ class InseeTestCase(BaseDBTestCase):
         
         series_list = self.db[constants.COL_SERIES].find(query)
         
+        #print(self.fetcher._data._series)
+        
         count = series_list.count()
         
-        self.assertEqual(count, DATASETS[dataset_code]['series_count'])
+        #print("count : ", len(self.fetcher._data._series))
         
-        query['key'] = "001654489" 
-        series_001654489 = self.db[constants.COL_SERIES].find_one(query)
-        self.assertIsNotNone(series_001654489)
+        self.assertEqual(count, 1)#DATASETS[dataset_code]['series_count'])
+        
+        # https://sdw-wsrest.ecb.europa.eu/service/data/EXR/M.NOK.EUR.SP00.A
+        query['key'] = "M.NOK.EUR.SP00.A" 
+        series_sample = self.db[constants.COL_SERIES].find_one(query)
+        self.assertIsNotNone(series_sample)
         
         #1990-01
-        self.assertEqual(series_001654489["values"][0], "139.22")
+        self.assertEqual(series_sample["values"][0], "8.651225")
 
         #2015-10
-        self.assertEqual(series_001654489["values"][-1], "105.61")
+        self.assertEqual(series_sample["values"][-1], "9.464159090909094")
         
-        frequency = series_001654489["frequency"]
-        start_date = str(pandas.Period(ordinal=series_001654489["start_date"], freq=frequency))
-        self.assertEqual(start_date, '1990-01')
+        frequency = series_sample["frequency"]
+        self.assertEqual(frequency, "M")
+        
+        start_date = str(pandas.Period(ordinal=series_sample["start_date"], freq=frequency))
+        self.assertEqual(start_date, '1999-01')
 
-        end_date = str(pandas.Period(ordinal=series_001654489["end_date"], freq=frequency))
-        self.assertEqual(end_date, '2015-10')
+        end_date = str(pandas.Period(ordinal=series_sample["end_date"], freq=frequency))
+        self.assertEqual(end_date, '2015-12')
+        
+        self.assertEqual(series_sample['dimensions'], {'SOURCE_AGENCY': '4F0', 'UNIT': 'NOK', 'UNIT_MULT': '0', 'CURRENCY': 'NOK', 'EXR_SUFFIX': 'A', 'EXR_TYPE': 'SP00', 'CURRENCY_DENOM': 'EUR', 'COLLECTION': 'A', 'DECIMALS': '4', 'FREQ': 'M'})
+        
         
     @unittest.skipIf(True, "TODO")
     def test_dimensions_to_dict(self):
         pass
-    
-    @httpretty.activate     
-    @mock.patch('dlstats.fetchers.insee.INSEE.upsert_dataset', mock_upsert_dataset)    
-    def test_invalid_series_key(self):
-
-        # nosetests -s -v dlstats.tests.fetchers.test_insee:InseeTestCase.test_invalid_series_key
-        
-        dataset_code = 'CNA-2010-CONSO-SI-A17'
-        
-        self._load_dataset(dataset_code)
-        self.insee.upsert_dataset(dataset_code)
-        series_list = self.insee.insee_data._series
-        self.assertEqual(len(series_list), DATASETS[dataset_code]['series_count'])
-        
-        series = series_list[0]
-        self.assertTrue('SECT-INST' in series['dimensions'])
     
     @unittest.skipIf(True, "TODO")
     def test_select_short_dimension(self):
@@ -215,41 +256,8 @@ class InseeTestCase(BaseDBTestCase):
         pass
     
     @httpretty.activate     
-    @mock.patch('dlstats.fetchers.insee.INSEE.upsert_dataset', mock_upsert_dataset)    
+    @mock.patch('dlstats.fetchers.ecb.ECB.upsert_dataset', mock_upsert_dataset)    
     @unittest.skipIf(True, "TODO")
     def test_is_updated(self):
+        pass
 
-        # nosetests -s -v dlstats.tests.fetchers.test_insee:InseeTestCase.test_is_updated
-
-        dataset_code = 'IPI-2010-A21'
-        
-        self._load_dataset(dataset_code)
-        self.insee.upsert_dataset(dataset_code)
-        _series = self.insee.insee_data._series
-        self.assertEqual(len(_series), DATASETS[dataset_code]['series_count'])
-        
-        self._load_dataset(dataset_code)
-        self.insee.upsert_dataset(dataset_code)
-        _series = self.insee.insee_data._series
-        self.assertEqual(len(_series), 0)
-
-        '''series avec un LAST_UPDATE > au dataset'''
-        query = {
-            'provider_name': self.insee.provider_name,
-            "dataset_code": dataset_code
-        }
-        new_datetime = datetime(2015, 12, 9)
-        result = self.db[constants.COL_DATASETS].update_one(query, {"$set": {'last_update': new_datetime}})
-        pprint(result.raw_result)
-        self._load_dataset(dataset_code)
-        self.insee.upsert_dataset(dataset_code)
-        _series = self.insee.insee_data._series
-        #pprint(_series)
-        for s in _series:
-            print(s['key'])
-        d = self.db[constants.COL_DATASETS].find_one(query)
-        print("dataset : ", d['last_update'])
-        self.assertEqual(len(_series), 11)
-        
-        
-        
