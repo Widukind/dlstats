eae467d6beca0b2e000f06863144f48eeb9d4685
Stéphane RAULT <stephane.rault@radicalspam.org>
2016-01-29 16:30:37 +0100

    Add options for Downloader Add requests-cache optional usage

--- dlstats/utils.py
+++ dlstats/utils.py
@@ -4,10 +4,23 @@ import time
 import os
 import logging
 import tempfile
+from io import StringIO
+import traceback
 
 import requests
 
+try:
+    import requests_cache
+    HAVE_REQUESTS_CACHE = True
+except ImportError:
+    HAVE_REQUESTS_CACHE = False
+
 logger = logging.getLogger(__name__)
+
+def last_error():
+    f = StringIO() 
+    traceback.print_exc(file=f)
+    return f.getvalue()
 
 class Downloader:
     
@@ -18,7 +31,8 @@ class Downloader:
     def __init__(self, url=None, filename=None, store_filepath=None, 
                  timeout=None, max_retries=0, 
                  replace=True, force_replace=True,
-                 headers={}):
+                 headers={},
+                 cache=None):
         
         self.url = url
         self.filename = filename
@@ -41,8 +55,11 @@ class Downloader:
         
         if os.path.exists(self.filepath) and not replace:
             raise Exception("filepath is already exist : %s" % self.filepath)
+
+        if HAVE_REQUESTS_CACHE and cache:
+            requests_cache.install_cache(**cache)
         
-    def _download(self):
+    def _download(self, raise_errors=True):
         
         #TODO: max_retries (self.max_retries)
         #TODO: analyse rate limit dans headers
@@ -56,27 +73,37 @@ class Downloader:
                                     verify=False,
                                     headers=self.headers)
 
-            if not response.ok:
-                msg = "download url[%s] - status_code[%s] - reason[%s]" % (self.url, 
-                                                                           response.status_code, 
-                                                                           response.reason)
-                logger.error(msg)
-                raise Exception(msg)
+            code = int(response.status_code)
             
+            if code >= 400:
+                msg = "download url[%s] - status_code[%s] - reason[%s]" % (self.url, 
+                                                                           code, 
+                                                                           response.reason)
+                if raise_errors:
+                    logger.error(msg)
+                    raise response.raise_for_status()
+                else:
+                    logger.warning(msg)
+                    return response
+
             with open(self.filepath, 'wb') as f:
                 for chunk in response.iter_content():
                     f.write(chunk)
 
             return response
         
-        except requests.exceptions.ConnectionError as err:
-            raise Exception("Connection Error")
-        except requests.exceptions.ConnectTimeout as err:
-            raise Exception("Connect Timeout")
-        except requests.exceptions.ReadTimeout as err:
-            raise Exception("Read Timeout")
+            """
+            except requests.exceptions.ConnectionError as err:
+                raise Exception("Connection Error")
+            except requests.exceptions.ConnectTimeout as err:
+                raise Exception("Connect Timeout")
+            except requests.exceptions.ReadTimeout as err:
+                raise Exception("Read Timeout")
+            """
         except Exception as err:
-            raise Exception("Not captured exception : %s" % str(err))            
+            logger.critical("Not captured exception : %s" % str(err))
+            raise
+            #raise Exception("Not captured exception : %s" % str(err))            
 
         end = time.time() - start
         logger.info("download file[%s] - END - time[%.3f seconds]" % (self.url, end))
@@ -103,7 +130,7 @@ class Downloader:
         
         if not os.path.exists(self.filepath):
             logger.info("not found file[%s] - download dataset url[%s]" % (self.filepath, self.url))
-            response = self._download()
+            response = self._download(raise_errors=False)
         else:
             logger.info("use local dataset file [%s]" % self.filepath)
         
