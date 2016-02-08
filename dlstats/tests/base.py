12394c88e5623d90945fba421fc6195746089dc5
Stéphane RAULT <stephane.rault@radicalspam.org>
2016-02-08 16:59:15 +0100

    refactoring base tests

--- dlstats/tests/base.py
+++ dlstats/tests/base.py
@@ -3,26 +3,23 @@
 import os
 import unittest
 
-from widukind_common.utils import get_mongo_db, create_or_update_indexes
-from widukind_common import tests_tools as utils
-
 from dlstats import constants
-
 from dlstats.tests import resources
 
 RESOURCES_DIR = os.path.abspath(os.path.dirname(resources.__file__))
 
 class BaseTestCase(unittest.TestCase):
-    
-    def setUp(self):
-        unittest.TestCase.setUp(self)
+    pass
 
-class BaseDBTestCase(BaseTestCase):
+class BaseDBTestCase(unittest.TestCase):
     """Tests with MongoDB
     """
 
     def setUp(self):
         BaseTestCase.setUp(self)
+
+        from widukind_common.utils import get_mongo_db, create_or_update_indexes
+        from widukind_common import tests_tools as utils
         
         db = get_mongo_db()
         self.db = db.client["widukind_test"] 
@@ -33,8 +30,10 @@ class BaseDBTestCase(BaseTestCase):
                 
         create_or_update_indexes(self.db, force_mode=True)
 
+        self._collections_is_empty()
+
     def _collections_is_empty(self):
-        self.assertTrue(self.db[constants.COL_PROVIDERS].count() <= 1)
+        self.assertEqual(self.db[constants.COL_PROVIDERS].count(), 0)
         self.assertEqual(self.db[constants.COL_CATEGORIES].count(), 0)
         self.assertEqual(self.db[constants.COL_DATASETS].count(), 0)
         self.assertEqual(self.db[constants.COL_SERIES].count(), 0)
