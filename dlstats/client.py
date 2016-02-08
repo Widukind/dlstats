e3cecdfd826fc699cd28187d770b4df464176b8d
Stéphane RAULT <stephane.rault@radicalspam.org>
2016-02-08 16:53:46 +0100

    add cache options for pandas and requests

--- dlstats/client.py
+++ dlstats/client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import tempfile
 import pprint
 import os
 import sys
@@ -12,8 +13,11 @@ import click
 from widukind_common.utils import get_mongo_url, get_mongo_client, configure_logging
 
 from dlstats import version
+from dlstats.constants import CACHE_URL
 
 DLSTATS_SETTINGS = dict(auto_envvar_prefix='DLSTATS')
+
+DEFAULT_PATH_REQUESTS_CACHE = os.path.abspath(os.path.join(tempfile.gettempdir(), "dlstats"))
 
 opt_mongo_url = click.option('--mongo-url',
                              envvar='WIDUKIND_MONGODB_URL',
@@ -50,12 +54,34 @@ opt_logger_conf = click.option('--log-config',
                                type=click.Path(exists=True), 
                                help='Logging config filepath')
 
+opt_cache_enable = click.option('-C', '--cache-enable', 
+                               is_flag=True,
+                               help='Enable global cache')
+
+opt_requests_cache_enable = click.option('--requests-cache-enable', 
+                               is_flag=True,
+                               help='Enable requests cache')
+
+opt_requests_cache_path = click.option('--requests-cache-path', 
+                               type=click.Path(exists=False),
+                               default=DEFAULT_PATH_REQUESTS_CACHE,
+                               show_default=True, 
+                               help='Path for requests cache')
+
+opt_requests_cache_expire = click.option('--requests-cache-expire', 
+                                default=60 * 60 * 4, 
+                                type=int, 
+                                help='Requests cache expire. default 4 hours. 0 for disabled')
+
 cmd_folder = os.path.abspath(
                     os.path.join(os.path.dirname(__file__), 'commands'))
 
 class Context(object):
     def __init__(self, mongo_url=None, verbose=False,
-                 log_level='ERROR', log_config=None, log_file=None,                 
+                 log_level='ERROR', log_config=None, log_file=None,
+                 cache_enable=False,  
+                 requests_cache_enable=None, requests_cache_path=None, 
+                 requests_cache_expire=None,               
                  debug=False, silent=False, pretty=False, quiet=False):
 
         self.mongo_url = mongo_url
@@ -64,6 +90,12 @@ class Context(object):
         self.silent = silent
         self.pretty = pretty
         self.quiet = quiet
+        
+        self.cache_enable = cache_enable
+        
+        self.requests_cache_enable = requests_cache_enable
+        self.requests_cache_path = requests_cache_path
+        self.requests_cache_expire = requests_cache_expire
         
         self.log_level = log_level
         self.log_config = log_config
@@ -78,16 +110,48 @@ class Context(object):
         self.fs_mongo = None
         
         self.logger = configure_logging(debug=self.debug, 
-                                #stdout_enable, 
                                 config_file=self.log_config, 
                                 level=self.log_level)
+        
         if self.log_file:
-            from logging import FileHandler
-            handler = FileHandler(filename=self.log_file)
-            if self.logger.hasHandlers():
-                handler.setFormatter(self.logger.handlers[0].formatter)
-            self.logger.addHandler(handler)
+            self._set_log_file()
+            
+        if self.cache_enable:
+            self._set_cache()
+            
+        if self.requests_cache_enable:
+            self._set_requests_cache()
 
+    def _set_log_file(self):
+        from logging import FileHandler
+        handler = FileHandler(filename=self.log_file)
+        if self.logger.hasHandlers():
+            handler.setFormatter(self.logger.handlers[0].formatter)
+        self.logger.addHandler(handler)
+        
+    def _set_cache(self):
+        from dlstats import cache
+        cache.configure_cache(cache_url=CACHE_URL)
+            
+    def _set_requests_cache(self):
+
+        cache_settings = {
+            "backend": 'sqlite', 
+            "cache_name": self.requests_cache_path, 
+            "expire_after": self.requests_cache_expire,
+            "include_get_headers": True
+        }
+        
+        if self.requests_cache_expire <= 0:
+            cache_settings["expire_after"] = None
+        
+        try:
+            import requests_cache
+            requests_cache.install_cache(**cache_settings)
+            self.log("Use requests cache in %s" % self.requests_cache_path)
+        except ImportError:
+            self.log_warn("requests-cache library is required for enable cache.")
+            
     def log(self, msg, *args):
         """Logs a message to stderr."""
         if self.quiet:
