bb4c1375082d68a78e194d3d1d3399eadc0d1b12
Stéphane RAULT <stephane.rault@radicalspam.org>
2016-02-08 16:10:38 +0100

    Add exception for interrupt data process

--- dlstats/errors.py
+++ dlstats/errors.py
@@ -4,6 +4,7 @@ class DlstatsException(Exception):
     def __init__(self, *args, **kwargs):
         self.provider_name = kwargs.pop("provider_name", None)
         self.dataset_code = kwargs.pop("dataset_code", None)
+        self.comments = kwargs.pop("comments", None)
         super().__init__(*args, **kwargs)
 
 class RejectFrequency(DlstatsException):
@@ -11,14 +12,17 @@ class RejectFrequency(DlstatsException):
     def __init__(self, *args, **kwargs):
         self.frequency = kwargs.pop("frequency", None)
         super().__init__(*args, **kwargs)
-        
+
+class InterruptProcessSeriesData(DlstatsException):
+    pass
+
 class RejectEmptySeries(DlstatsException):
     pass
 
 class RejectUpdatedDataset(DlstatsException):
     """Reject if dataset is updated
     """
-
+    
 class RejectUpdatedSeries(DlstatsException):
     """Reject if series is updated
     """
