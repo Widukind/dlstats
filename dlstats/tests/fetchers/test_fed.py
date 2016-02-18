62c03ea1b09f9e276986e6f61a43da6fc3c2f2ec
Stéphane RAULT <stephane.rault@radicalspam.org>
2016-02-18 22:21:19 +0100

    fixes and cleaning

--- dlstats/tests/fetchers/test_fed.py
+++ dlstats/tests/fetchers/test_fed.py
@@ -5,7 +5,7 @@ import os
 from dlstats.fetchers.fed import FED as Fetcher
 from dlstats.fetchers.fed import DATASETS as FETCHER_DATASETS
 
-from unittest import mock
+import unittest
 import httpretty
 
 from dlstats.tests.base import RESOURCES_DIR as BASE_RESOURCES_DIR
@@ -14,37 +14,35 @@ from dlstats.tests.resources import xml_samples
 
 RESOURCES_DIR = os.path.abspath(os.path.join(BASE_RESOURCES_DIR, "fed"))
 
-"""
-['G19 - CCOUT', 'G19 - TERMS']
-{'attrs': {'agency': 'FRB', 'id': 'CCOUT'},
- 'dsd_id': 'G19 - CCOUT',
- 'global_id': ['G19'],
- 'id': 'G19 - CCOUT',
- 'name': 'G.19 - Consumer Credit - Consumer Credit Outstanding'}
-{'attrs': {'agency': 'FRB', 'id': 'TERMS'},
- 'dsd_id': 'G19 - TERMS',
- 'global_id': ['G19'],
- 'id': 'G19 - TERMS',
- 'name': 'G.19 - Consumer Credit - Terms of Credit Outstanding'}
- 
- 
-"""
-
 LOCAL_DATASETS_UPDATE = {
     "G19-TERMS": {
         #'categories_root': ['BAL', 'BF', 'ERID', 'FA', 'HF', 'IA', 'IR', 'MSRB', 'PEI'],
-        "concept_keys": ['UNIT', 'CURRENCY', 'UNIT_MULT', 'OBS_STATUS', 'TERMS', 'ISSUE', 'FREQ', 'SERIES_NAME'],
-        "codelist_keys": ['UNIT', 'CURRENCY', 'UNIT_MULT', 'OBS_STATUS', 'TERMS', 'ISSUE', 'FREQ', 'SERIES_NAME'],
+        "concept_keys": ['CURRENCY', 'FREQ', 'ISSUE', 'OBS_STATUS', 'SERIES_NAME', 'TERMS', 'UNIT', 'UNIT_MULT'],
+        "codelist_keys": ['CURRENCY', 'FREQ', 'ISSUE', 'OBS_STATUS', 'SERIES_NAME', 'TERMS', 'UNIT', 'UNIT_MULT'],
         "codelist_count": {
-            "UNIT": 413,
-            "CURRENCY": 200,
-            "UNIT_MULT": 9,
-            "OBS_STATUS": 4,
-            "TERMS": 8,
+            "CURRENCY": 1,
+            "FREQ": 1,
             "ISSUE": 2,
-            "FREQ": 50,
+            "OBS_STATUS": 1,
             "SERIES_NAME": 0,
+            "TERMS": 8,
+            "UNIT": 3,
+            "UNIT_MULT": 1,
         },
+        "dimension_keys": ['ISSUE', 'TERMS', 'FREQ'],
+        "dimension_count": {
+            "ISSUE": 2,
+            "TERMS": 8,
+            "FREQ": 1,
+        },
+        "attribute_keys": ['OBS_STATUS', 'CURRENCY', 'UNIT', 'UNIT_MULT', 'SERIES_NAME'],
+        "attribute_count": {
+            "OBS_STATUS": 1,
+            "CURRENCY": 1,
+            "UNIT": 3,
+            "UNIT_MULT": 1,
+            "SERIES_NAME": 0,
+        }
     }
 }
 
@@ -74,6 +72,7 @@ class FetcherTestCase(BaseFetcherTestCase):
                           match_querystring=False)
 
     @httpretty.activate
+    @unittest.skipUnless('FULL_TEST' in os.environ, "Skip - no full test")
     def test_load_datasets_first(self):
 
         dataset_code = "G19-TERMS"
@@ -81,6 +80,7 @@ class FetcherTestCase(BaseFetcherTestCase):
         self.assertLoadDatasetsFirst(datasets_filter=[dataset_code])
 
     @httpretty.activate     
+    @unittest.skipUnless('FULL_TEST' in os.environ, "Skip - no full test")
     def test_load_datasets_update(self):
 
         dataset_code = "G19-TERMS"
@@ -95,10 +95,11 @@ class FetcherTestCase(BaseFetcherTestCase):
 
     @httpretty.activate     
     def test_upsert_dataset_g19_terms(self):
+
+        # nosetests -s -v dlstats.tests.fetchers.test_fed:FetcherTestCase.test_upsert_dataset_g19_terms
         
         dataset_code = "G19-TERMS"
         self._load_files(dataset_code)
-             
         self.assertProvider()
         self.assertDataset(dataset_code)
         self.assertSeries(dataset_code)
